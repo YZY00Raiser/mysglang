2c250eae1402d49ccf6194754a656e9ca2ea0ebd
YZY00Raiser <2452658175@qq.com>
2026-02-07 14:51:43 +0800

    update

---
+++ test/registered/ascend/basic_function/parameter/debug.py
@@ -0,0 +1,49 @@
+import os
+import unittest
+import numpy
+from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
+from sglang.test.test_utils import (
+    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+    DEFAULT_URL_FOR_TEST,
+    CustomTestCase,
+    popen_launch_server,
+)
+
+
+class TestDebugTensorInputFile(CustomTestCase):
+    def test_tensor_input_file(self):
+        vector = numpy.array([1001, 1002, 1003, 1004, 1005, 1006, 1007])
+        numpy.save("./input_tensor.npy", vector)
+        other_args = [
+            "--debug-tensor-dump-input-file",
+            "./input_tensor.npy",
+            "--attention-backend",
+            "ascend",
+            "--disable-cuda-graph",
+        ]
+        out_log_file = open("./tensor_input_out_log.txt", "w+", encoding="utf-8")
+        err_log_file = open("./tensor_input_err_log.txt", "w+", encoding="utf-8")
+        popen_launch_server(
+            LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH,
+            DEFAULT_URL_FOR_TEST,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=other_args,
+            return_stdout_stderr=(out_log_file, err_log_file),
+        )
+        print("----------------------------out==0----------------------------")
+        print(out_log_file)
+        print("----------------------------err==0----------------------------")
+        print(err_log_file)
+        err_log_file.seek(0)
+        content = err_log_file.read()
+        self.assertTrue(len(content) > 0)
+        # self.assertIn("The server is fired up and ready to roll!", content)
+        out_log_file.close()
+        err_log_file.close()
+        os.remove("./tensor_input_out_log.txt")
+        os.remove("./tensor_input_err_log.txt")
+        os.remove("./input_tensor.npy")
+
+
+if __name__ == "__main__":
+    unittest.main()
