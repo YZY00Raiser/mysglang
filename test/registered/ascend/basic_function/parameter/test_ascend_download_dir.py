2a3d68ce62cc6a6f2c146442917fcb0b49eb39fc
YZY00Raiser <2452658175@qq.com>
2026-02-09 11:25:55 +0800

    update

--- test/registered/ascend/basic_function/parameter/test_ascend_download_dir.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_download_dir.py
@@ -35,13 +35,14 @@ class TestDownloadDir(CustomTestCase):
             "ascend",
             "--disable-cuda-graph",
         ]
+        run_command(f"rm -rf {cls.download_dir}")
         cls.process = popen_launch_server(
             cls.model,
             DEFAULT_URL_FOR_TEST,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
             other_args=other_args,
         )
-        run_command(f"rm -rf {cls.download_dir}")
+
     @classmethod
     def tearDownClass(cls):
         kill_process_tree(cls.process.pid)
