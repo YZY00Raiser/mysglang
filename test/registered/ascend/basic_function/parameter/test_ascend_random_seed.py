2f81127b59b9196a7132fc3f3c305c5cacc4dc4f
YZY00Raiser <2452658175@qq.com>
2026-02-11 18:37:09 +0800

    update

--- test/registered/ascend/basic_function/parameter/test_ascend_random_seed.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_random_seed.py
@@ -14,8 +14,9 @@ from sglang.test.ci.ci_register import register_npu_ci
 register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 
-class TestRandomSeed(CustomTestCase):
-    """Testcase：Verify set --random-seed parameter, the inference request is successfully processed.
+class TestRandomSeedZero(CustomTestCase):
+    """Testcase：Verify set --random-seed parameter, different random_seed will affect the model's output (response.text)
+    and when random_seed is the same, the response.text will same.
 
        [Test Category] Parameter
        [Test Target] --random-seed
@@ -24,27 +25,19 @@ class TestRandomSeed(CustomTestCase):
     random_seed = 0
 
     @classmethod
-    def get_server_args(cls):
-        """Return the arguments for the server launch. Override in subclasses."""
-        other_args = (
-            [
-                "--attention-backend",
-                "ascend",
-                "--disable-cuda-graph",
-                "--random-seed",
-                cls.random_seed,
-            ]
-
-        )
-        return other_args
-
-    @classmethod
     def setUpClass(cls):
+        other_args = [
+            "--attention-backend",
+            "ascend",
+            "--disable-cuda-graph",
+            "--random-seed",
+            cls.random_seed,
+        ]
         cls.process = popen_launch_server(
             cls.model,
             DEFAULT_URL_FOR_TEST,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=cls.get_server_args(),
+            other_args=other_args,
         )
 
     @classmethod
@@ -52,81 +45,29 @@ class TestRandomSeed(CustomTestCase):
         kill_process_tree(cls.process.pid)
 
     def test_random_seed(self):
-        response = requests.post(
-            f"{DEFAULT_URL_FOR_TEST}/generate",
-            json={
-                "text": "The capital of France is",
-                "sampling_params": {
-                    "temperature": 1,
-                    "max_new_tokens": 32,
+        response_text1 = None
+        response_text2 = None
+        for i in range(2):
+            response = requests.post(
+                f"{DEFAULT_URL_FOR_TEST}/generate",
+                json={
+                    "text": "The capital of France is",
+                    "sampling_params": {
+                        "temperature": 1,
+                        "max_new_tokens": 32,
+                    },
                 },
-            },
-        )
-        self.assertEqual(
-            response.status_code, 200, "The request status code is not 200."
-        )
-        print("------------------respo1111111111111nseeeeee---------------------------------")
-        print(response.json())
-        self.assertIn(
-            "Paris", response.text, "The inference result does not include Paris."
-        )
+            )
+            self.assertEqual(response.status_code, 200)
+            if i == 0:
+                response_text1 = response.json()["text"]
+            else:
+                response_text2 = response.json()["text"]
+        self.assertEqual(response_text1, response_text2)
 
-class TestRandomSeedOne(CustomTestCase):
-    """Testcase：Verify set --random-seed parameter, the inference request is successfully processed.
 
-           [Test Category] Parameter
-           [Test Target] --random-seed
-           """
-    model = LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
-    random_seed = 0
-
-    @classmethod
-    def get_server_args(cls):
-        """Return the arguments for the server launch. Override in subclasses."""
-        other_args = (
-            [
-                "--attention-backend",
-                "ascend",
-                "--disable-cuda-graph",
-                "--random-seed",
-                cls.random_seed,
-            ]
-
-        )
-        return other_args
-
-    @classmethod
-    def setUpClass(cls):
-        cls.process = popen_launch_server(
-            cls.model,
-            DEFAULT_URL_FOR_TEST,
-            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=cls.get_server_args(),
-        )
-
-    @classmethod
-    def tearDownClass(cls):
-        kill_process_tree(cls.process.pid)
-
-    def test_random_seed(self):
-        response = requests.post(
-            f"{DEFAULT_URL_FOR_TEST}/generate",
-            json={
-                "text": "The capital of France is",
-                "sampling_params": {
-                    "temperature": 1,
-                    "max_new_tokens": 32,
-                },
-            },
-        )
-        self.assertEqual(
-            response.status_code, 200, "The request status code is not 200."
-        )
-        print("------------------respo1111111111111nseeeeee---------------------------------")
-        print(response.json())
-        self.assertIn(
-            "Paris", response.text, "The inference result does not include Paris."
-        )
+class TestRandomSeedOne(TestRandomSeedZero):
+    random_seed = 1
 
 
 if __name__ == "__main__":
