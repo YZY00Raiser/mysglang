15661264734c0f0f66ce8abae0ab6c79f4e9f0b1
YZY00Raiser <2452658175@qq.com>
2026-02-11 22:11:39 +0800

    update

--- test/registered/ascend/basic_function/parameter/test_ascend_request_length_validation.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_request_length_validation.py
@@ -1,8 +1,5 @@
 import unittest
-
 import openai
-from openai import completions
-
 from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
 from sglang.srt.utils import kill_process_tree
 from sglang.test.test_utils import (
