b59422ebe80824e22306814972d353368ae0bad3
YZY00Raiser <2452658175@qq.com>
2026-02-11 21:49:18 +0800

    update

--- test/registered/ascend/basic_function/parameter/test_ascend_random_seed.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_random_seed.py
@@ -60,16 +60,8 @@ class TestRandomSeedZero(CustomTestCase):
             self.assertEqual(response.status_code, 200)
             if i == 0:
                 response_text1 = response.json()["text"]
-                print("-------0000000000000000000-------------")
-                print(response_text1)
-                print("-------seedddd-------------")
-                print(self.random_seed)
             else:
                 response_text2 = response.json()["text"]
-                print("-------1111111111111111111-------------")
-                print(response_text2)
-                print("-------seedddd-------------")
-                print(self.random_seed)
         self.assertEqual(response_text1, response_text2)
 
 
