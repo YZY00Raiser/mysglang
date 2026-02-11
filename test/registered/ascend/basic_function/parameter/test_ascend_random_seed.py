5d01305d0164f991532deb350601810e98730575
YZY00Raiser <2452658175@qq.com>
2026-02-11 21:01:38 +0800

    update

--- test/registered/ascend/basic_function/parameter/test_ascend_random_seed.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_random_seed.py
@@ -56,7 +56,7 @@ class TestRandomSeedZero(CustomTestCase):
                 json={
                     "text": "The capital of France is",
                     "sampling_params": {
-                        "temperature": 0.1,
+                        "temperature": 1,
                         "max_new_tokens": 32,
                     },
                 },
@@ -73,7 +73,7 @@ class TestRandomSeedZero(CustomTestCase):
         self.assertEqual(response_text1, response_text2)
 
 class TestRandomSeedOne(TestRandomSeedZero):
-    random_seed = 1
+    random_seed = 42
 
 
 # class TestRandomSeed(CustomTestCase):
