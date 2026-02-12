d9915fd88b241491f3b35b53a0f0b3c1de698a91
YZY00Raiser <2452658175@qq.com>
2026-02-12 08:31:59 +0800

    update

--- test/registered/ascend/basic_function/parameter/test_ascend_request_length_validation.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_request_length_validation.py
@@ -1,5 +1,6 @@
 import unittest
 import openai
+import requests
 from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
 from sglang.srt.utils import kill_process_tree
 from sglang.test.test_utils import (
@@ -94,82 +95,63 @@ class TestRequestLengthValidation(CustomTestCase):
             "max_completion_tokens is too large",
             str(cm.exception),
         )
-class TestRequestLengthValidationGenerate(CustomTestCase):
 
+
+class TestRequestLengthValidationGenerate(CustomTestCase):
+    """
+    Test threshold
+    """
     model = LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
 
     @classmethod
     def setUpClass(cls):
-        cls.base_url = DEFAULT_URL_FOR_TEST
-        cls.api_key = "sk-123456"
+        other_args = [
+            "--attention-backend",
+            "ascend",
+            "--disable-cuda-graph",
+            "--max-total-tokens", "1000",
+            "--context-length", "1000"
+        ]
         cls.process = popen_launch_server(
             cls.model,
-            cls.base_url,
+            DEFAULT_URL_FOR_TEST,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            api_key=cls.api_key,
-            other_args=["--max-total-tokens", "1000", "--context-length", "1000"],
+            other_args=other_args,
         )
 
     @classmethod
     def tearDownClass(cls):
         kill_process_tree(cls.process.pid)
 
-    def test_input_length_no_longer_than_context_length_success(self):
-        client = openai.Client(api_key=self.api_key, base_url=f"{self.base_url}/v1")
-        long_text = "hello " * 500
-        response = client.chat.completions.create(
-            model=self.model,
-            messages=[
-                {"role": "user", "content": long_text},
-            ],
-            temperature=0,
-        )
-        completions_tokens = response.usage.completion_tokens
-        self.assertGreater(completions_tokens, 0)
+    def test_context_length_success(self):
+        input_ids = [100] * 1000
+        response = requests.post(
+            f"{DEFAULT_URL_FOR_TEST}/generate",
+            json={
+                "input_ids":  input_ids,
+                "sampling_params": {
+                    "temperature": 0,
 
-    def test_input_length_longer_than_context_length(self):
-        client = openai.Client(api_key=self.api_key, base_url=f"{self.base_url}/v1")
-        long_text = "hello " * 1200
-        with self.assertRaises(openai.BadRequestError) as cm:
-            client.chat.completions.create(
-                model=self.model,
-                messages=[
-                    {"role": "user", "content": long_text},
-                ],
-                temperature=0,
-            )
-        self.assertIn("is longer than the model's context length", str(cm.exception))
-
-    def test_not_longer_max_tokens_validation_success(self):
-        client = openai.Client(api_key=self.api_key, base_url=f"{self.base_url}/v1")
-        long_text = "hello "
-        response = client.chat.completions.create(
-            model=self.model,
-            messages=[
-                {"role": "user", "content": long_text},
-            ],
-            temperature=0,
-            max_tokens=800,
+                },
+            },
         )
-        completions_tokens = response.usage.completion_tokens
-        self.assertGreater(completions_tokens, 0)
+        self.assertEqual(response.status_code, 200)
 
-    def test_longer_max_tokens_validation(self):
-        client = openai.Client(api_key=self.api_key, base_url=f"{self.base_url}/v1")
-        long_text = "hello "
-        with self.assertRaises(openai.BadRequestError) as cm:
-            client.chat.completions.create(
-                model=self.model,
-                messages=[
-                    {"role": "user", "content": long_text},
-                ],
-                temperature=0,
-                max_tokens=1001,
-            )
-        self.assertIn(
-            "max_completion_tokens is too large",
-            str(cm.exception),
+
+    def test_max_token_success(self):
+        input_ids = [100] * 999
+        response = requests.post(
+            f"{DEFAULT_URL_FOR_TEST}/generate",
+            json={
+                "input_ids": input_ids,
+                "sampling_params": {
+                    "temperature": 0,
+                    "max_new_tokens": 1,
+                },
+            },
         )
+        self.assertEqual(response.status_code, 200)
+
 
 if __name__ == "__main__":
     unittest.main()
