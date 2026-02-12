7924cb2fbdc7bc8cfa98a5ad76109de3cdd9af89
YZY00Raiser <2452658175@qq.com>
2026-02-12 11:31:46 +0800

    update

--- test/registered/ascend/basic_function/parameter/test_ascend_fim_completion.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_fim_completion.py
@@ -85,7 +85,7 @@ class TestFimCompletion(CustomTestCase):
 class TestFimCompletionJson(TestFimCompletion):
     other_args = [
         "--completion-template",
-        "deepseek_coder.json",
+        "mysglang/test/registered/ascend/basic_function/parameter/deepseek_coder.json",
         "--attention-backend",
         "ascend",
         "--disable-cuda-graph",
