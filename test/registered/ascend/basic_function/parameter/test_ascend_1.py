caaa1a4f7d49ea620f3d8276cfea18161154ab5d
YZY00Raiser <2452658175@qq.com>
2026-02-13 08:45:58 +0800

    update

--- test/registered/ascend/basic_function/parameter/test_ascend_1.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_1.py
@@ -1,9 +1,8 @@
 import unittest
-from types import SimpleNamespace
+
 import requests
-from sglang.srt.utils import kill_process_tree
 from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
-from sglang.test.run_eval import run_eval
+from sglang.srt.utils import kill_process_tree
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
@@ -15,69 +14,40 @@ from sglang.test.ci.ci_register import register_npu_ci
 register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 
-class TestEnableMultimodalNonMlm(CustomTestCase):
-    """Testcase: Verify that when the --enable-multimodal parameter is set, mmlu accuracy greater than or equal 0.37
+class TestEnableCacheReport(CustomTestCase):
+    """Testcase：Verify set --enable-cache-report, sent openai request prompt_tokens_details will return cached_tokens.
 
-        [Test Category] Parameter
-        [Test Target] --enable-multimodal
-        """
-    model = LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
-    base_url = DEFAULT_URL_FOR_TEST
-    score_with_param = None
-    score_without_param = None
-    accuracy=0.37
+       [Test Category] Parameter
+       [Test Target] --enable-cache-report
+       """
 
-    @classmethod
-    def setUpClass(cls):
-        other_args = [
-            "--trust-remote-code",
-            "--enable-multimodal",
-            "--mem-fraction-static",
-            "0.8",
-            "--attention-backend",
-            "ascend",
-            "--disable-cuda-graph",
-        ]
+    def test_enable_cache_report(self):
+        for i in range(2):
+            response = requests.post(
+                f"{DEFAULT_URL_FOR_TEST}/v1/completions",
+                json={
+                    "prompt": "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, ",
+                    "max_tokens": 260,
 
-        cls.process = popen_launch_server(
-            cls.model,
-            cls.base_url,
-            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=other_args,
-        )
-
-    @classmethod
-    def tearDownClass(cls):
-        kill_process_tree(cls.process.pid)
-
-    def test_enable_multimodal_func(self):
-        response = requests.post(
-            f"{DEFAULT_URL_FOR_TEST}/generate",
-            json={
-                "text": "The capital of France is",
-                "sampling_params": {
-                    "temperature": 0,
-                    "max_new_tokens": 32,
                 },
-            },
-        )
-        print("-----------------------------response---------------------------------------------------")
-        print(response.json())
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("Paris", response.text)
-
-    def test_mmlu(self):
-        args = SimpleNamespace(
-            base_url=self.base_url,
-            model=self.model,
-            eval_name="mmlu",
-            num_examples=64,
-            num_threads=32,
-        )
-
-        metrics = run_eval(args)
-        self.assertGreaterEqual(metrics["score"], self.accuracy)
+            )
+            self.assertEqual(response.status_code, 200)
+            if i == 1:
+                cached_tokens = response.json()["usage"]['prompt_tokens_details']['cached_tokens']
+                print(cached_tokens)
+                self.assertEqual(256, cached_tokens)
 
 
 if __name__ == "__main__":
-    unittest.main(verbosity=2)
+    unittest.main()
