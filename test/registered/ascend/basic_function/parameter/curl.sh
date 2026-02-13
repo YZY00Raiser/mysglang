5b47df90113b3870c230f852b0a6a7a636343019
YZY00Raiser <2452658175@qq.com>
2026-02-13 22:15:46 +0800

    update

---
+++ test/registered/ascend/basic_function/parameter/curl.sh
@@ -0,0 +1,2 @@
+curl --location 'http://172.22.3.19:6688/generate' --header 'Content-Type:application/json' --data '{"text":"The capital of France is", "sampling_params":{"temperature":0, "max_new_tokens":100}}'
+
