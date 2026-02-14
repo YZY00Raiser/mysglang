e28f5bb7d7ad0a4675002b2524a4e2def4aacf96
YZY00Raiser <2452658175@qq.com>
2026-02-14 18:18:21 +0800

    update

--- test/registered/ascend/basic_function/parameter/R2.sh
+++ test/registered/ascend/basic_function/parameter/R2.sh
@@ -3,5 +3,5 @@ python -m sglang_router.launch_router \
     --pd-disaggregation \
     --prefill http://172.22.3.34:8000 8996 \
     --decode http://172.22.3.188:8000 \
-    --host 172.22.3.19 \
+    --host 172.22.3.34 \
     --port 6688
