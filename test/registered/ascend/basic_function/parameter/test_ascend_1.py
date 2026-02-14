05f53b02dc86f56137e399353cc2d233c1d001a4
YZY00Raiser <2452658175@qq.com>
2026-02-14 11:59:12 +0800

    update

--- test/registered/ascend/basic_function/parameter/test_ascend_1.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_1.py
@@ -10,44 +10,12 @@ register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 
 class TestEnableCacheReport(CustomTestCase):
-    """Testcase：Verify set --enable-cache-report, sent openai request prompt_tokens_details will return cached_tokens.
+    """Testcase：Verify use DeepSeek V3.2  Prefix set enable-hierarchical-cache, not set --disable-radix-cache
+    send two same requests with 600 token the second response's cached_tokens equal 512.
 
-       [Test Category] Parameter
-       [Test Target] --enable-cache-report
-       """
-
-    # def test_enable_cache_report(self):
-    #     URL_FOR_TEST = "http://127.0.0.1:6666"
-    #     for i in range(2):
-    #         response = requests.post(
-    #             f"{URL_FOR_TEST}/generate",
-    #             json={
-    #                 "prompt": "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, ",
-    #                 "max_tokens": 1,
-    #
-    #             },
-    #         )
-    #         self.assertEqual(response.status_code, 200)
-    #         print("------------------------------------response.json()---------------------------------------------")
-    #         print(response.json())
-    #
-    #         # if i == 1:
-    #         #     cached_tokens = response.json()["usage"]['prompt_tokens_details']['cached_tokens']
-    #         #     print(cached_tokens)
-    #         #     self.assertEqual(256, cached_tokens)
-
-
+    [Test Category] model
+    [Test Target] enable-hierarchical-cache
+    """
     def test_enable_hierarchical(self):
         DEFAULT_URL_FOR_TEST="http://172.22.3.19:6688"
         print("==============startoooo====================================")
