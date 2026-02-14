564e295b1c3d008c905b1fc8f94e7df2c5036147
YZY00Raiser <2452658175@qq.com>
2026-02-14 09:13:38 +0800

    update

--- test/registered/ascend/basic_function/parameter/P19.sh
+++ test/registered/ascend/basic_function/parameter/P19.sh
@@ -14,6 +14,6 @@ python3 -m sglang.launch_server \
 --chunked-prefill-size -1 \
 --watchdog-timeout 9000 \
 --max-prefill-tokens 68000 \
---enable-hierarchical-cache \
+--enable-hierarchical-cache
 
 
