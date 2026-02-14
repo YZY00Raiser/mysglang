a5870f1487dd3aeb5868d9ef3ac8f5726b928702
YZY00Raiser <2452658175@qq.com>
2026-02-14 15:49:37 +0800

    update

--- test/registered/ascend/basic_function/parameter/test_ascend_2.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_2.py
@@ -1,60 +1,40 @@
 import unittest
-from types import SimpleNamespace
+
 import requests
-from sglang.srt.utils import kill_process_tree
-from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
-from sglang.test.run_eval import run_eval
 from sglang.test.test_utils import (
-    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-    DEFAULT_URL_FOR_TEST,
     CustomTestCase,
-    popen_launch_server,
 )
 from sglang.test.ci.ci_register import register_npu_ci
 
 register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
 
 
-class TestEnableMultimodalNonMlm(CustomTestCase):
-    """Testcase: Verify that when the --enable-multimodal parameter is set, mmlu accuracy greater than or equal 0.37
+class TestEnableCacheReport(CustomTestCase):
+    """Testcase：Verify use DeepSeek V3.2  Prefix set enable-hierarchical-cache, not set --disable-radix-cache
+    send two same requests with 600 token the second response's cached_tokens equal 512.
 
-        [Test Category] Parameter
-        [Test Target] --enable-multimodal
-        """
-    model = LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
-    base_url = DEFAULT_URL_FOR_TEST
-    score_with_param = None
-    score_without_param = None
-    accuracy=0.37
-
-    def test_enable_multimodal_func(self):
+    [Test Category] model
+    [Test Target] enable-hierarchical-cache
+    """
+    def test_enable_hierarchical(self):
+        DEFAULT_URL_FOR_TEST="http://172.22.3.19:6688"
+        print("==============startoooo====================================")
+        input_ids = [1] * 300
         response = requests.post(
             f"{DEFAULT_URL_FOR_TEST}/generate",
             json={
-                "text": "The capital of France is",
+                "input_ids": input_ids,
                 "sampling_params": {
                     "temperature": 0,
-                    "max_new_tokens": 32,
+                    "max_tokens": 200,
                 },
             },
         )
-        print("-----------------------------response---------------------------------------------------")
+
+        print("==============respob=====================================")
         print(response.json())
+        print("=============finshhhhhhhhhhhhhhh====================================")
         self.assertEqual(response.status_code, 200)
-        self.assertIn("Paris", response.text)
-
-    def test_mmlu(self):
-        args = SimpleNamespace(
-            base_url=self.base_url,
-            model=self.model,
-            eval_name="mmlu",
-            num_examples=64,
-            num_threads=32,
-        )
-
-        metrics = run_eval(args)
-        self.assertGreaterEqual(metrics["score"], self.accuracy)
-
 
 if __name__ == "__main__":
-    unittest.main(verbosity=2)
+    unittest.main()
