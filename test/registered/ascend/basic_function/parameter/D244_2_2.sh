ff2916c4ff0218a8bc0a6a8a4708025e7507de64
YZY00Raiser <2452658175@qq.com>
2026-02-14 13:41:22 +0800

    update

---
+++ test/registered/ascend/basic_function/parameter/D244_2_2.sh
@@ -0,0 +1,15 @@
+export ASCEND_MF_STORE_URL="tcp://172.22.3.19:24667"
+python3 -m sglang.launch_server \
+--model-path /root/.cache/modelscope/hub/models/vllm-ascend/DeepSeek-V3.2-W8A8 \
+--mem-fraction-static 0.8 \
+--host 172.22.3.244 --port 8000 \
+--disaggregation-mode decode \
+--disaggregation-transfer-backend ascend \
+--trust-remote-code \
+--tp-size 16 \
+--attention-backend ascend \
+--device npu \
+--chunked-prefill-size -1 \
+--disaggregation-decode-enable-offload-kvcache \
+--hicache-storage-backend file
+
