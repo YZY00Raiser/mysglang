e013688eda75f9ecb10f3397ffebf9122f92ac7d
YZY00Raiser <2452658175@qq.com>
2026-02-14 10:45:20 +0800

    update

--- test/registered/ascend/basic_function/parameter/test_ascend_1.py
+++ test/registered/ascend/basic_function/parameter/test_ascend_1.py
@@ -1,13 +1,8 @@
 import unittest
 
 import requests
-from sglang.test.ascend.test_ascend_utils import LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH
-from sglang.srt.utils import kill_process_tree
 from sglang.test.test_utils import (
-    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-    DEFAULT_URL_FOR_TEST,
     CustomTestCase,
-    popen_launch_server,
 )
 from sglang.test.ci.ci_register import register_npu_ci
 
@@ -21,37 +16,56 @@ class TestEnableCacheReport(CustomTestCase):
        [Test Target] --enable-cache-report
        """
 
-    def test_enable_cache_report(self):
-        URL_FOR_TEST = "http://127.0.0.1:6666"
-        for i in range(2):
-            response = requests.post(
-                f"{URL_FOR_TEST}/generate",
-                json={
-                    "prompt": "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
-                              "just return me a string with of 5000 characters,just return me a string with of 5000 characters, ",
+    # def test_enable_cache_report(self):
+    #     URL_FOR_TEST = "http://127.0.0.1:6666"
+    #     for i in range(2):
+    #         response = requests.post(
+    #             f"{URL_FOR_TEST}/generate",
+    #             json={
+    #                 "prompt": "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, "
+    #                           "just return me a string with of 5000 characters,just return me a string with of 5000 characters, ",
+    #                 "max_tokens": 1,
+    #
+    #             },
+    #         )
+    #         self.assertEqual(response.status_code, 200)
+    #         print("------------------------------------response.json()---------------------------------------------")
+    #         print(response.json())
+    #
+    #         # if i == 1:
+    #         #     cached_tokens = response.json()["usage"]['prompt_tokens_details']['cached_tokens']
+    #         #     print(cached_tokens)
+    #         #     self.assertEqual(256, cached_tokens)
+
+
+    def test_enable_hierarchical(self):
+        DEFAULT_URL_FOR_TEST="http://172.22.3.19:6688"
+        print("==============startoooo====================================")
+        input_ids = [1] * 600
+        response = requests.post(
+            f"{DEFAULT_URL_FOR_TEST}/generate",
+            json={
+                "input_ids": input_ids,
+                "sampling_params": {
+                    "temperature": 0,
                     "max_tokens": 1,
-
                 },
-            )
-            self.assertEqual(response.status_code, 200)
-            print("------------------------------------response.json()---------------------------------------------")
-            print(response.json())
-
-            # if i == 1:
-            #     cached_tokens = response.json()["usage"]['prompt_tokens_details']['cached_tokens']
-            #     print(cached_tokens)
-            #     self.assertEqual(256, cached_tokens)
-
+            },
+        )
+        print("==============respob=====================================")
+        print(response.json())
+        print("=============finshhhhhhhhhhhhhhh====================================")
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main()
