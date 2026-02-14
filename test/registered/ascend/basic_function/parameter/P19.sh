dbb01553c5b47ed8d653c2b5e7b75d46de1bfd08
YZY00Raiser <2452658175@qq.com>
2026-02-14 08:44:03 +0800

    update

---
+++ test/registered/ascend/basic_function/parameter/P19.sh
@@ -0,0 +1,22 @@
+export ASCEND_MF_STORE_URL="tcp://172.22.3.19:24667"
+python3 -m sglang.launch_server \--model-path /root/.cache/modelscope/hub/models/vllm-ascend/DeepSeek-V2-Lite-W8A8 \
+--mem-fraction-static 0.8 \
+--host 172.22.3.19 \
+--port 8000 \
+--disaggregation-mode prefill \
+--disaggregation-bootstrap-port 8996 \
+--disaggregation-transfer-backend ascend \
+--trust-remote-code \
+--tp-size 16 \
+--attention-backend ascend \
+--disable-radix-cache \
+--disable-cuda-graph \
+--device npu \
+--quantization modelslim \
+--chunked-prefill-size -1 \
+--watchdog-timeout 9000 \
+--disable-radix-cache \
+--max-prefill-tokens 68000 \
+--moe-a2a-backend deepep \
+--deepep-mode auto \
+--quantization modelslim
