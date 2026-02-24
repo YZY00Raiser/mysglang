f171341e59285ba34d7cfb1e3e8af2c7baaf2ddc
YZY00Raiser <2452658175@qq.com>
2026-02-24 11:38:47 +0800

    update

---
+++ test/registered/ascend/basic_function/parameter/D244_Qween.sh
@@ -0,0 +1,17 @@
+export ASCEND_MF_STORE_URL="tcp://172.22.3.34:24667"
+python3 -m sglang.launch_server \
+--model-path /root/.cache/modelscope/hub/models/Qwen/Qwen3-32B \
+--mem-fraction-static 0.75 \
+--host 172.22.3.244 --port 8000 \
+--disaggregation-mode decode \
+--disaggregation-transfer-backend ascend \
+--trust-remote-code \
+--tp-size 16 \
+--attention-backend ascend \
+--device npu \
+--chunked-prefill-size -1 \
+--quantization modelslim \
+--page-size 128 \
+--disaggregation-decode-enable-offload-kvcache \
+--hicache-storage-backend file
+
---
+++ test/registered/ascend/basic_function/parameter/P34_Qween.sh
@@ -0,0 +1,21 @@
+export ASCEND_MF_STORE_URL="tcp://172.22.3.34:24667"
+python3 -m sglang.launch_server \
+--model-path /root/.cache/modelscope/hub/models/Qwen/Qwen3-32B \
+--mem-fraction-static 0.75 \
+--host 172.22.3.34 \
+--port 8000 \
+--disaggregation-mode prefill \
+--disaggregation-bootstrap-port 8996 \
+--disaggregation-transfer-backend ascend \
+--trust-remote-code \
+--tp-size 16 \
+--attention-backend ascend \
+--device npu \
+--chunked-prefill-size -1 \
+--watchdog-timeout 9000 \
+--max-prefill-tokens 68000 \
+--quantization modelslim \
+--page-size 128 \
+--enable-hierarchical-cache \
+--hicache-storage-backend file
+
