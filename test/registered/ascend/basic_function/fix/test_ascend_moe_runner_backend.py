b26c1facb427cf4b92a6d77a2c1913fc6e97c151
YZY00Raiser <2452658175@qq.com>
2026-02-25 18:04:02 +0800

    update

--- test/registered/ascend/basic_function/fix/test_ascend_moe_runner_backend.py
+++ test/registered/ascend/basic_function/fix/test_ascend_moe_runner_backend.py
@@ -3,7 +3,7 @@ import unittest
 import requests
 
 from sglang.srt.utils import kill_process_tree
-from sglang.test.ascend.test_ascend_utils import DEEPSEEK_V3_2_W8A8_WEIGHTS_PATH
+from sglang.test.ascend.test_ascend_utils import KIMI_VL_A3B_INSTRUCT_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
@@ -22,7 +22,7 @@ class TestMoreRunnerBackendTriton(CustomTestCase):
     [Test Target] --moe-runner-backend
     """
 
-    model = DEEPSEEK_V3_2_W8A8_WEIGHTS_PATH
+    model = KIMI_VL_A3B_INSTRUCT_WEIGHTS_PATH
     moe_runner_backend = "triton"
 
     @classmethod
