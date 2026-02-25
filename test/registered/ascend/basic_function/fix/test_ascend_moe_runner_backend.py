0726851f7a38f93eeb041e1920b0492e3d4f1ac7
YZY00Raiser <2452658175@qq.com>
2026-02-25 17:25:12 +0800

    update

--- test/registered/ascend/basic_function/fix/test_ascend_moe_runner_backend.py
+++ test/registered/ascend/basic_function/fix/test_ascend_moe_runner_backend.py
@@ -3,7 +3,7 @@ import unittest
 import requests
 
 from sglang.srt.utils import kill_process_tree
-from sglang.test.ascend.test_ascend_utils import OLMOE_1B_7B_0924_WEIGHTS_PATH
+from sglang.test.ascend.test_ascend_utils import DEEPSEEK_V3_2_W8A8_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
@@ -22,7 +22,7 @@ class TestMoreRunnerBackendTriton(CustomTestCase):
     [Test Target] --moe-runner-backend
     """
 
-    model = OLMOE_1B_7B_0924_WEIGHTS_PATH
+    model = DEEPSEEK_V3_2_W8A8_WEIGHTS_PATH
     moe_runner_backend = "triton"
 
     @classmethod
@@ -35,6 +35,10 @@ class TestMoreRunnerBackendTriton(CustomTestCase):
                 "--attention-backend",
                 "ascend",
                 "--disable-cuda-graph",
+                "--mem-fraction-static",
+                "0.8",
+                "--tp-size",
+                "16",
                 "--moe-runner-backend",
                 cls.moe_runner_backend,
             ],
