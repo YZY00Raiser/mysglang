a36e253234d6c940ec3147a1d469a11cd297686d
YZY00Raiser <2452658175@qq.com>
2026-02-26 10:01:30 +0800

    update

--- test/registered/ascend/basic_function/fix/test_ascend_moe_runner_backend.py
+++ test/registered/ascend/basic_function/fix/test_ascend_moe_runner_backend.py
@@ -26,26 +26,21 @@ class TestMoreRunnerBackendTriton(CustomTestCase):
     moe_runner_backend = "triton"
 
     @classmethod
-    def get_server_args(cls):
-        other_args = [
-            "--attention-backend",
-            "ascend",
-            "--disable-cuda-graph",
-            "--trust-remote-code",
-            "--moe-runner-backend",
-            cls.moe_runner_backend,
-            "--base-gpu-id",
-            "7",
-        ]
-        return other_args
-
-    @classmethod
     def setUpClass(cls):
         cls.process = popen_launch_server(
             cls.model,
             DEFAULT_URL_FOR_TEST,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=cls.get_server_args(),
+            other_args=[
+                "--attention-backend",
+                "ascend",
+                "--disable-cuda-graph",
+                "--trust-remote-code",
+                "--moe-runner-backend",
+                cls.moe_runner_backend,
+                "--base-gpu-id",
+                "7",
+            ]
         )
 
     @classmethod
