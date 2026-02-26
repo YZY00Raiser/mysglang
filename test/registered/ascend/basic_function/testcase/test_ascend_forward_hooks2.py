65644eb416bd1bdf9c504f7f90e054d0fb489180
YZY00Raiser <2452658175@qq.com>
2026-02-26 20:14:47 +0800

    update

--- test/registered/ascend/basic_function/testcase/test_ascend_forward_hooks2.py
+++ test/registered/ascend/basic_function/testcase/test_ascend_forward_hooks2.py
@@ -125,9 +125,9 @@ class TestSetForwardHooks(CustomTestCase):
         os.remove(cls.hook_log_file_name)
 
     def test_enable_multimodal_func(self):
-        # with self.assertRaises(Exception) as ctx:
-        self._launch_server()
-        # self.assertIn("Server process exited with code -9", str(ctx.exception))
+        with self.assertRaises(Exception) as ctx:
+            self._launch_server()
+        self.assertIn("Server process exited with code -2", str(ctx.exception))
         print("-----------------------------launch_server------------------------------------------")
         self.hook_log_file.seek(0)
         hook_content = self.hook_log_file.read()
