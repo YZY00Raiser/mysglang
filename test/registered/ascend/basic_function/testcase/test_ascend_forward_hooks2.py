e3744ce68d4c2fc4b1e933c5e6406f3589c8af25
YZY00Raiser <2452658175@qq.com>
2026-02-26 11:53:15 +0800

    update

---
+++ test/registered/ascend/basic_function/testcase/test_ascend_forward_hooks2.py
@@ -0,0 +1,78 @@
+import json
+import unittest
+import requests
+from sglang.srt.utils import kill_process_tree
+from sglang.test.ascend.test_ascend_utils import QWEN3_32B_WEIGHTS_PATH
+from sglang.test.test_utils import (
+    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+    DEFAULT_URL_FOR_TEST,
+    CustomTestCase,
+    popen_launch_server,
+)
+from sglang.test.ci.ci_register import register_npu_ci
+
+register_npu_ci(est_time=400, suite="nightly-4-npu-a3", nightly=True)
+
+
+class TestEnableMultimodalNonMlm(CustomTestCase):
+    """Testcase: Verify set --forward-hooks parameter, can identify the set hook function and the inference request is successfully processed.
+
+    [Test Category] Parameter
+    [Test Target] --forward-hooks
+    """
+    model = QWEN3_32B_WEIGHTS_PATH
+    hooks_spec = [
+        {
+            "name": "qwen_first_layer_attn_monitor",
+            "target_modules": ["model.layers.0.self_attn"],
+            "hook_factory": "monitor:create_attention_monitor_factory",
+            "config": {
+                "layer_index": 0
+            }
+        }
+    ]
+
+    @classmethod
+    def setUpClass(cls):
+        other_args = [
+            "--trust-remote-code",
+            "--mem-fraction-static",
+            "0.8",
+            "--attention-backend",
+            "ascend",
+            "--disable-cuda-graph",
+            "--tp-size",
+            "4",
+            "--forward-hooks",
+            json.dumps(cls.hooks_spec),
+            "--base-gpu-id", "4",
+        ]
+        cls.process = popen_launch_server(
+            cls.model,
+            DEFAULT_URL_FOR_TEST,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=other_args,
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_enable_multimodal_func(self):
+        response = requests.post(
+            f"{DEFAULT_URL_FOR_TEST}/generate",
+            json={
+                "text": "The capital of France is",
+                "sampling_params": {
+                    "temperature": 0,
+                    "max_new_tokens": 32,
+                },
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Paris", response.text)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
