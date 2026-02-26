1246dbd2ddfa395c4f7ad45706b30fa284a1f678
YZY00Raiser <2452658175@qq.com>
2026-02-26 14:07:11 +0800

    update

--- test/registered/ascend/basic_function/testcase/test_ascend_forward_hooks2.py
+++ test/registered/ascend/basic_function/testcase/test_ascend_forward_hooks2.py
@@ -44,7 +44,7 @@ class TestEnableMultimodalNonMlm(CustomTestCase):
             "--tp-size",
             "4",
             "--forward-hooks",
-            cls.hooks_spec,
+            json.dumps(cls.hooks_spec),
             "--base-gpu-id", "4",
         ]
         cls.process = popen_launch_server(
