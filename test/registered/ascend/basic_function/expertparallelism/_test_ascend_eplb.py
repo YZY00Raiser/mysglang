e315ce7da352a6d8878f06f0a4d3363b2183981b
YZY00Raiser <2452658175@qq.com>
2026-03-07 18:14:18 +0800

    update

--- test/registered/ascend/basic_function/expertparallelism/_test_ascend_eplb.py
+++ test/registered/ascend/basic_function/expertparallelism/_test_ascend_eplb.py
@@ -15,26 +15,20 @@ from sglang.test.test_utils import (
 from sglang.test.ci.ci_register import register_npu_ci
 
 register_npu_ci(est_time=200, suite="nightly-16-npu-a3", nightly=True)
-model_path = "/home/weights/DeepSeek-Coder-V2-Lite-Instruct"
 
 
-class _BaseTestDynamicEPLB(CustomTestCase):
+class TestEplbAlgorithm(CustomTestCase):
     """
-    Testcase：Verify the correctness of the function when eplb is enabled and the accuracy of DeepSeek-R1-W8A8 on MMLU
-    dataset
+    Testcase：Verify the correctness of the inference service when --eplb-algorithm is set
 
     [Test Category] Parameter
-    [Test Target] --enable-eplb， --ep-num-redundant-experts, --eplb-rebalance-num-iterations,
-    --expert-distribution-recorder-buffer-size, --enable-expert-distribution-metrics,
-    --expert-distribution-recorder-mode, --ep-dispatch-algorithm
+    [Test Target] --eplb-algorithm
     """
-
-    extra_args = []
 
     @classmethod
     def setUpClass(cls):
         # cls.model = MODEL_PATH
-        cls.model = model_path
+        cls.model = "/home/weights/DeepSeek-Coder-V2-Lite-Instruct"
         cls.base_url = DEFAULT_URL_FOR_TEST
         cls.process = popen_launch_server(
             cls.model,
@@ -44,39 +38,14 @@ class _BaseTestDynamicEPLB(CustomTestCase):
                 "--trust-remote-code",
                 "--tp-size",
                 "2",
-                "--dp-size",
+                "--ep-size",
                 "2",
                 "--attention-backend",
                 "ascend",
-                "--quantization",
-                "modelslim",
-                "--mem-fraction-static",
-                "0.8",
-                "--enable-dp-attention",
-                "--moe-a2a-backend",
-                "deepep",
-                "--deepep-mode",
-                "normal",
                 "--disable-cuda-graph",
-                "--enable-eplb",
-                "--ep-num-redundant-experts",
-                "2",
-                "--eplb-rebalance-num-iterations",
-                "50",
-                "--expert-distribution-recorder-buffer-size",
-                "50",
-                "--enable-expert-distribution-metrics",
-                "--expert-distribution-recorder-mode",
-                "stat",
-                "--ep-dispatch-algorithm",
-                "static",
-                *cls.extra_args,
+                "--mem-fraction-static",
+                "0.7",
             ],
-            env={
-                "SGL_ENABLE_JIT_DEEPGEMM": "0",
-                "HCCL_BUFFSIZE": "500",
-                **os.environ,
-            },
         )
 
     @classmethod
@@ -97,93 +66,6 @@ class _BaseTestDynamicEPLB(CustomTestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertIn("Paris", response.text)
-
-    # def test_mmlu(self):
-    #     args = SimpleNamespace(
-    #         base_url=self.base_url,
-    #         model=self.model,
-    #         eval_name="mmlu",
-    #         num_examples=8,
-    #         num_threads=32,
-    #     )
-    #
-    #     metrics = run_eval(args)
-    #     self.assertGreater(metrics["score"], 0.5)
-
-
-# class TestDynamicEPLBSimple(_BaseTestDynamicEPLB):
-#     pass
-
-
-# class TestDynamicEPLBMultiChunk(_BaseTestDynamicEPLB):
-#     extra_args = ["--eplb-rebalance-layers-per-chunk", "1"]
-
-
-# class TestStaticEPLB(CustomTestCase):
-#     def test_save_expert_distribution_and_init_expert_location(self):
-#         os.environ["SGL_ENABLE_JIT_DEEPGEMM"] = "0"
-#         os.environ["HCCL_BUFFSIZE"] = "500"
-#
-#         with tempfile.TemporaryDirectory() as tmp_dir:
-#             engine_kwargs = dict(
-#                 model_path=model_path,
-#                 trust_remote_code=True,
-#                 attention_backend="ascend",
-#                 quantization="modelslim",
-#                 mem_fraction_static=0.9,
-#                 deepep_mode="normal",
-#                 ep_num_redundant_experts=16,
-#                 enable_dp_attention=True,
-#                 moe_a2a_backend="deepep",
-#                 disable_cuda_graph=True,
-#                 expert_distribution_recorder_mode="stat",
-#                 tp_size=16,
-#                 dp_size=1,
-#                 log_level="info",
-#                 enable_expert_distribution_metrics=True,
-#             )
-#
-#             print(f"Action: start engine")
-#             os.environ["SGLANG_EXPERT_DISTRIBUTION_RECORDER_DIR"] = tmp_dir
-#             engine = sgl.Engine(
-#                 **engine_kwargs,
-#                 disable_overlap_schedule=True,
-#             )
-#             engine.start_expert_distribution_record()
-#             self._assert_engine_generate_correct(engine)
-#
-#             print(f"Action: dump_expert_distribution_record")
-#             engine.dump_expert_distribution_record()
-#             snapshot_path = list(Path(tmp_dir).glob("*.pt"))[0]
-#             assert snapshot_path is not None
-#             print(f"{snapshot_path=}")
-#
-#             print(f"Action: shutdown engine")
-#             engine.shutdown()
-#             del engine
-#
-#             print(f"Action: start engine with init_expert_location")
-#             engine = sgl.Engine(
-#                 **engine_kwargs,
-#                 init_expert_location=str(snapshot_path),
-#                 port=21000,
-#                 ep_dispatch_algorithm="static",
-#             )
-#             self._assert_engine_generate_correct(engine)
-#             print(f"Action: shutdown engine")
-#             engine.shutdown()
-#             del engine
-#
-#     def _assert_engine_generate_correct(self, engine: sgl.Engine):
-#         output = engine.generate(
-#             prompt=["1+1=2, 2+2=4", "One plus one is two, two plus two is four"],
-#             sampling_params=dict(max_new_tokens=8, temperature=0.0),
-#         )
-#         print(f"engine.generate {output=}")
-#         self.assertEqual(
-#             [x["text"] for x in output],
-#             [", 4+4=8,", ", four plus four is eight, eight"],
-#         )
 
 
 if __name__ == "__main__":
