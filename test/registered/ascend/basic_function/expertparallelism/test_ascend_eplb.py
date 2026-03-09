7a843150e18d9a58cb1cde208a30607d27bd9692
YZY00Raiser <2452658175@qq.com>
2026-03-09 09:19:09 +0800

    update

--- test/registered/ascend/basic_function/expertparallelism/test_ascend_eplb.py
+++ test/registered/ascend/basic_function/expertparallelism/test_ascend_eplb.py
@@ -28,7 +28,9 @@ class TestEplbAlgorithm(CustomTestCase):
     @classmethod
     def setUpClass(cls):
         # cls.model = MODEL_PATH
-        cls.model = "/home/weights/DeepSeek-Coder-V2-Lite-Instruct"
+        # cls.model = "/home/weights/DeepSeek-Coder-V2-Lite-Instruct"
+        cls.model = "/home/weights/deepseek-ai/DeepSeek-Coder-V2-Lite-Instruct"
+
         cls.base_url = DEFAULT_URL_FOR_TEST
         cls.process = popen_launch_server(
             cls.model,
