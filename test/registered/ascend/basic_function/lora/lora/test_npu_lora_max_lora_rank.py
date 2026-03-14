80305de6d4d1b58bf8dc2bff867ebeb0cc4ac553
YZY00Raiser <2452658175@qq.com>
2026-03-14 17:00:31 +0800

    update

--- test/registered/ascend/basic_function/lora/lora/test_npu_lora_max_lora_rank.py
+++ test/registered/ascend/basic_function/lora/lora/test_npu_lora_max_lora_rank.py
@@ -4,10 +4,10 @@ import unittest
 import requests
 
 from sglang.srt.utils import kill_process_tree
-from sglang.test.ascend.test_ascend_utils import (
-    LLAMA_3_2_1B_INSTRUCT_TOOL_CALLING_LORA_WEIGHTS_PATH,
-    LLAMA_3_1_8B_INSTRUCT_WEIGHTS_PATH,
-)
+# from sglang.test.ascend.test_ascend_utils import (
+#     LLAMA_3_2_1B_INSTRUCT_TOOL_CALLING_LORA_WEIGHTS_PATH,
+#     LLAMA_3_1_8B_INSTRUCT_WEIGHTS_PATH,
+# )
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
@@ -17,6 +17,9 @@ from sglang.test.test_utils import (
 )
 
 register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
+LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH = "/home/weights/LLM-Research/Llama-3.2-1B-Instruct"
+LLAMA_3_2_1B_INSTRUCT_TOOL_CALLING_LORA_WEIGHTS_PATH = "/home/weights/codelion/Llama-3.2-1B-Instruct-tool-calling-lora"
+LLAMA_3_2_1B_INSTRUCT_TOOL_FAST_LORA_WEIGHTS_PATH = "/home/weights/codelion/FastLlama-3.2-LoRA"
 
 
 class TestLoraMaxLoraRank(CustomTestCase):
