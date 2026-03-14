31ad6c60027245507607738bb9f59cc3fee31e63
YZY00Raiser <2452658175@qq.com>
2026-03-14 11:25:26 +0800

    update

--- test/registered/ascend/basic_function/lora/test_npu_lora_max_lora_rank.py
+++ test/registered/ascend/basic_function/lora/test_npu_lora_max_lora_rank.py
@@ -88,6 +88,7 @@ class TestLoraMaxLoraRank(CustomTestCase):
 
     lora_a = LLAMA_3_2_1B_INSTRUCT_TOOL_CALLING_LORA_WEIGHTS_PATH
     max_lora_rank = "64"
+
     def test_max_loaded_loras_error(self):
         error_message = "The number of LoRA paths should not exceed max_loaded_loras."
         other_args = [
@@ -102,6 +103,29 @@ class TestLoraMaxLoraRank(CustomTestCase):
             "ascend",
             "--disable-cuda-graph",
         ]
+        self.process = popen_launch_server(
+            LLAMA_3_2_1B_WEIGHTS_PATH,
+            DEFAULT_URL_FOR_TEST,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=other_args,
+        )
+        response = requests.post(
+            f"{DEFAULT_URL_FOR_TEST}/generate",
+            json={
+                "text": "The capital of France is",
+                "sampling_params": {
+                    "temperature": 0,
+                    "max_new_tokens": 32,
+                },
+                "lora_path": "lora_a",
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Paris", response.text)
+
+        kill_process_tree(self.process.pid)
+
+        '''
         out_log_file = open("./cache_out_log.txt", "w+", encoding="utf-8")
         err_log_file = open("./cache_err_log.txt", "w+", encoding="utf-8")
         try:
@@ -123,6 +147,9 @@ class TestLoraMaxLoraRank(CustomTestCase):
             err_log_file.close()
             os.remove("./cache_out_log.txt")
             os.remove("./cache_err_log.txt")
+
+        '''
+
 
 if __name__ == "__main__":
     unittest.main()
