c2155bdd119c90f18e5914bdee961c32a3c9cb12
YZY00Raiser <2452658175@qq.com>
2026-03-26 14:10:19 +0800

    update

--- test/registered/ascend/basic_function/lora/lora_pr/test_ascend_api_v1_completions.py
+++ test/registered/ascend/basic_function/lora/lora_pr/test_ascend_api_v1_completions.py
@@ -15,7 +15,7 @@ from sglang.test.test_utils import (
 )
 
 register_npu_ci(est_time=400, suite="nightly-2-npu-a3", nightly=True)
-# QWEN3_30B_A3B_WEIGHTS_PATH=
+QWEN3_30B_A3B_WEIGHTS_PATH="/home/weights/Qwen/Qwen3-30B-A3B"
 LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH = "/home/weights/LLM-Research/Llama-3.2-1B-Instruct"
 LLAMA_3_2_1B_INSTRUCT_TOOL_CALLING_LORA_WEIGHTS_PATH = "/home/weights/codelion/Llama-3.2-1B-Instruct-tool-calling-lora"
 LLAMA_3_2_1B_INSTRUCT_TOOL_FAST_LORA_WEIGHTS_PATH = "/home/weights/codelion/FastLlama-3.2-LoRA"
