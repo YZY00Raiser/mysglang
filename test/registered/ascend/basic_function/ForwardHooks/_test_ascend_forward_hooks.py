670e2126a3a94e77287b09cb21c0298d84164e24
YZY00Raiser <2452658175@qq.com>
2026-03-30 19:39:28 +0800

    update

--- test/registered/ascend/basic_function/ForwardHooks/_test_ascend_forward_hooks.py
+++ test/registered/ascend/basic_function/ForwardHooks/_test_ascend_forward_hooks.py
@@ -5,9 +5,10 @@ import unittest
 import requests
 
 from sglang.srt.utils import kill_process_tree
-from sglang.test.ascend.test_ascend_utils import (
-    QWEN3_32B_WEIGHTS_PATH,
-)
+# from sglang.test.ascend.test_ascend_utils import (
+#     HOOK_FUNCTION_PATH,
+#     QWEN3_32B_WEIGHTS_PATH,
+# )
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
@@ -22,6 +23,39 @@ register_npu_ci(
     nightly=True,
 )
 
+import logging
+import time
+
+
+def create_attention_monitor_factory(config):
+    # hook factory
+    layer_index = config.get("layer_index", 0)
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    def attention_monitor_hook(module, inputs, output):
+        # The actual hook function is called during the forward propagation of the self-attention layer.
+        timestamp = time.time()
+
+        hidden_states = inputs[1] if inputs else None
+
+        monitor_record = {
+            "timestamp": timestamp,
+            "layer_index": layer_index,
+            "module_type": type(module).__name__,
+            "inputs": hidden_states.sum(-1)[:5] if hidden_states is not None else None,
+            "outputs": output.sum(-1)[:5],
+        }
+
+        logging.info(f"hook effect: {monitor_record}")
+
+        return output
+
+    return attention_monitor_hook
+
 
 class TestSetForwardHooks(CustomTestCase):
     """Testcase: Verify set --forward-hooks parameter, can identify the set hook function
@@ -31,8 +65,8 @@ class TestSetForwardHooks(CustomTestCase):
     [Test Target] --forward-hooks
     """
 
-    hook_function_path = "sglang.test.ascend.test_ascend_utils:create_attention_monitor_factory_hook"
-    model = QWEN3_32B_WEIGHTS_PATH
+    hook_function_path = "sglang.test.test_ascend_forward_hooks:create_attention_monitor_factory"
+    model = "/home/weights/Qwen/Qwen3-32B"
     hooks_spec = [
         {
             "name": "qwen_first_layer_attn_monitor",
