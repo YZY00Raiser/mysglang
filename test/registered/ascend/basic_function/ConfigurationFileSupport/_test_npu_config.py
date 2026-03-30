26adfafb9b8ac0c88eb2bcb560967a9e8b655687
YZY00Raiser <2452658175@qq.com>
2026-03-30 13:29:50 +0800

    update

--- test/registered/ascend/basic_function/ConfigurationFileSupport/_test_npu_config.py
+++ test/registered/ascend/basic_function/ConfigurationFileSupport/_test_npu_config.py
@@ -22,7 +22,9 @@ register_npu_ci(
     nightly=True,
 )
 
-
+CONFIG_YAML_PATH = (
+    "/data/y30082119/mysglang/test/registered/ascend/basic_function/ConfigurationFileSupport/config.yaml"
+)
 class TestConfig(CustomTestCase):
     """Testcase: Verify set --config parameter, can identify the set config and inference request is successfully processed.
 
