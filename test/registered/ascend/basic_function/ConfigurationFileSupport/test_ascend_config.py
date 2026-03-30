4c4c34d58988d5a633156d69c596e79eb772d993
YZY00Raiser <2452658175@qq.com>
2026-03-30 14:07:47 +0800

    update

--- test/registered/ascend/basic_function/ConfigurationFileSupport/test_ascend_config.py
+++ test/registered/ascend/basic_function/ConfigurationFileSupport/test_ascend_config.py
@@ -1,67 +1,62 @@
+import os
+import subprocess
 import unittest
 
 import requests
 
 from sglang.srt.utils import kill_process_tree
-# from sglang.test.ascend.test_ascend_utils import (
-#     CONFIG_YAML_PATH,
-#     popen_launch_server_config,
-# )
-from sglang.test.ascend.test_ascend_utils import (
-    QWEN3_32B_WEIGHTS_PATH,
-)
+# from sglang.test.ascend.test_ascend_utils import popen_launch_server
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
     CustomTestCase,
-    popen_launch_server,
+    popen_launch_server, _create_clean_subprocess_env, _wait_for_server_health,
 )
 
-register_npu_ci(
-    est_time=400,
-    suite="nightly-4-npu-a3",
-    nightly=True,
-)
-CONFIG_YAML_PATH = (
-    "/data/y30082119/mysglang/test/registered/ascend/basic_function/ConfigurationFileSupport/config.yaml"
-)
-'''
-class TestConfig(CustomTestCase):
+register_npu_ci(est_time=400, suite="nightly-4-npu-a3", nightly=True)
+
+MODEL_PATH = "/home/weights/Qwen3-0.6B"
+PATH = "/home/l30081563/prtest/sglang/test/registered/ascend/basic_function/parameter"
+CONFIG_YAML_PATH = f"{PATH}/config.yaml"
+CONFIG_INVALID_YAML_PATH = f"{PATH}/config_invalid.yaml"
+
+
+class TestAscendConfig(CustomTestCase):
     """Testcase: Verify set --config parameter, can identify the set config and inference request is successfully processed.
 
     [Test Category] Parameter
     [Test Target] --config
     """
 
-    model = None
     config = CONFIG_YAML_PATH
 
     @classmethod
-    def _build_other_args(cls):
-        return [
-            "--config",
-            cls.config,
-        ]
+    def setUpClass(cls):
+        cls.model = MODEL_PATH
+        cls.base_url = DEFAULT_URL_FOR_TEST
 
-    @classmethod
-    def _launch_server(cls):
-        other_args = cls._build_other_args()
-        cls.process = popen_launch_server_config(
-            cls.model,
-            DEFAULT_URL_FOR_TEST,
-            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=other_args,
-        )
+        # TODO：或许应该在这里生成config文件
+
+        cls.process = cls._launch_server_with_config_yaml(cls.config, cls.base_url, DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH)
+        # command = [
+        #     "python3",
+        #     "-m",
+        #     "sglang.launch_server",
+        #     "--config",
+        #     cls.config,
+        # ]
+        # cls.process = subprocess.Popen(command, stdout=None, stderr=None,
+        #                            env=_create_clean_subprocess_env(os.environ.copy()))
+        # _wait_for_server_health(cls.process, cls.base_url, None, DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH)
 
     @classmethod
     def tearDownClass(cls):
         kill_process_tree(cls.process.pid)
 
     def test_config(self):
-        self._launch_server()
         response = requests.post(
-            f"{DEFAULT_URL_FOR_TEST}/generate",
+            f"{self.base_url}/generate",
             json={
                 "text": "The capital of France is",
                 "sampling_params": {
@@ -74,44 +69,101 @@ class TestConfig(CustomTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn("Paris", response.text)
 
+    @classmethod
+    def _launch_server_with_config_yaml(cls, config_file, url, timeout):
+        command = [
+            "python3",
+            "-m",
+            "sglang.launch_server",
+            "--config",
+            config_file,
+        ]
+        process = subprocess.Popen(command, stdout=None, stderr=None,
+                                       env=_create_clean_subprocess_env(os.environ.copy()))
+        _wait_for_server_health(process, url, None, timeout)
+        return process
 
-'''
 
 
-class TestConfigPriority(CustomTestCase):
-    """Testcase: Verify set the parameter set in the command line have a higher priority than set in config.yaml,
-    set false model path in the command, set right model path in the config.yaml,
-    will use false model path service start fail .
+
+class TestAscendConfigInValidConfigFileType(CustomTestCase):
+    """Testcase: Verify set --config non yaml file format the service start fail.
 
     [Test Category] Parameter
     [Test Target] --config
     """
 
-    # model = "/data/Qwen/Qwen3-32B"
-    model = QWEN3_32B_WEIGHTS_PATH
-    config = CONFIG_YAML_PATH
+    invalid_config_file_list = [
+        "config.ini",
+        "config.txt",
+        "config.xml",
+    ]
+
+    # for config in test_cases:
 
     @classmethod
-    def _launch_server(cls):
-        cls.process = popen_launch_server(
-            cls.model,
-            DEFAULT_URL_FOR_TEST,
-            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=["--config", cls.config, ],
-        )
+    def setUpClass(cls):
+        cls.model = MODEL_PATH
+        cls.base_url = DEFAULT_URL_FOR_TEST
 
     @classmethod
     def tearDownClass(cls):
-        kill_process_tree(cls.process.pid)
+        pass
 
     def test_config(self):
-        with self.assertRaises(Exception) as ctx:
-            self._launch_server()
-        self.assertIn(
-            "Server process exited with code 1. Check server logs for errors.",
-            str(ctx.exception),
-        )
+        process = None
+        for config in self.invalid_config_file_list:
+            try:
+            # with self.assertRaises(Exception) as ctx:
+
+                self.other_args = [
+                    "--config",
+                    config,
+                ]
+
+
+                process = popen_launch_server(
+                    self.model,
+                    self.base_url,
+                    timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+                    other_args=self.other_args,
+                )
+            except Exception as e:
+                self.assertIn(
+                    "Server process exited with code 1. Check server logs for errors.",
+                    str(e),
+                )
+                print(e)
+            finally:
+                if process:
+                    kill_process_tree(process.pid)
+
+
+
+
+
+# class TestConfigParamValidation(TestConfig):
+#     """Testcase: Verify set exception param in config file the service start fail.
+#
+#     [Test Category] Parameter
+#     [Test Target] --config
+#     """
+#
+#     config = CONFIG_VALID_YAML_PATH
+#
+#     def test_config(self):
+#         with self.assertRaises(Exception) as ctx:
+#             self._launch_server()
+#         self.assertIn(
+#             "Server process exited with code 2. Check server logs for errors.",
+#             str(ctx.exception),
+#         )
 
 
 if __name__ == "__main__":
-    unittest.main()
+    # unittest.main()
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+    suite.addTests(loader.loadTestsFromTestCase(TestAscendConfig))
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
