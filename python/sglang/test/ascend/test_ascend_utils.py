63dd15f1e33eb5cbcfbc7f58682c7b7db678b482
YZY00Raiser <2452658175@qq.com>
2026-03-31 09:28:43 +0800

    update

--- python/sglang/test/ascend/test_ascend_utils.py
+++ python/sglang/test/ascend/test_ascend_utils.py
@@ -652,9 +652,8 @@ def popen_launch_server_with_config_yaml(config_file, base_url, timeout):
     # parsed_url = urlparse(base_url)
     # host = parsed_url.hostname
     # port = parsed_url.port
+    # _, host, port = base_url.split(":")
     # host = host[2:]
-    _, host, port = base_url.split(":")
-    host = host[2:]
     command = [
         "python3",
         "-m",
