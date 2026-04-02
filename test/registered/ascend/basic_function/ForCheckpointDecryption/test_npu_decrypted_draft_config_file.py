5046986b26913059cdcb0fbb3ea1625c96dbfae1
YZY00Raiser <2452658175@qq.com>
2026-04-03 01:14:35 +0800

    update

---
+++ test/registered/ascend/basic_function/ForCheckpointDecryption/test_npu_decrypted_draft_config_file.py
@@ -0,0 +1,104 @@
+import json
+import os
+import unittest
+
+import requests
+
+from sglang.srt.utils import kill_process_tree
+# from sglang.test.ascend.test_ascend_utils import (
+#     QWEN3_32B_WEIGHTS_PATH,
+#     QWEN3_32B_EAGLE3_WEIGHTS_PATH
+# )
+from sglang.test.ci.ci_register import register_npu_ci
+from sglang.test.test_utils import (
+    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+    DEFAULT_URL_FOR_TEST,
+    CustomTestCase,
+    popen_launch_server,
+)
+
+register_npu_ci(
+    est_time=400,
+    suite="nightly-4-npu-a3",
+    nightly=True,
+)
+QWEN3_32B_WEIGHTS_PATH = "/home/weights/Qwen/Qwen3-32B"
+QWEN3_32B_EAGLE3_WEIGHTS_PATH = "/home/weights/Qwen3-32B-Eagle3"
+
+
+class TestSetForwardHooks(CustomTestCase):
+    """Testcase: Verify set --decrypted-config-file, --decrypted-draft-config-file parameter, set non exist config.json,
+    will use the non exist config.json and the service startup failed
+
+    [Test Category] Parameter
+    [Test Target] --decrypted-config-file, --decrypted-draft-config-file
+    """
+
+    model = QWEN3_32B_WEIGHTS_PATH
+
+    @classmethod
+    def setUpClass(cls):
+        other_args = [
+            "--trust-remote-code",
+            "--attention-backend",
+            "ascend",
+            "--device",
+            "npu",
+            "--max-running-requests",
+            "1",
+            "--disable-radix-cache",
+            "--chunked-prefill-size",
+            "-1",
+            "--max-prefill-tokens",
+            "1024",
+            "--speculative-algorithm",
+            "EAGLE3",
+            "--speculative-draft-model-path",
+            "QWEN3_32B_EAGLE3_WEIGHTS_PATH",
+            "--speculative-num-steps",
+            "3",
+            "--speculative-eagle-topk",
+            "1",
+            "--speculative-num-draft-tokens",
+            "4",
+            "--base-gpu-id",
+            "12",
+            "--tp-size",
+            "2",
+            "--mem-fraction-static",
+            "0.68",
+            "--disable-cuda-graph",
+            "--dtype", "bfloat16",
+            "--decrypted-config-file",
+            "/home/weights/Qwen3-32B/non_exist/config.json",
+            "--decrypted-draft-config-file",
+            "/home/weights/Qwen3-32B-Eagle3/non_exist/config.json"
+        ]
+        cls.process = popen_launch_server(
+            cls.model,
+            DEFAULT_URL_FOR_TEST,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=other_args,
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_decrypted_draft_config_file(self):
+        response = requests.post(
+            f"{DEFAULT_URL_FOR_TEST}/generate",
+            json={
+                "text": "The capital of France is",
+                "sampling_params": {
+                    "temperature": 0.8,
+                    "max_new_tokens": 32,
+                },
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Paris", response.text)
+
+
+if __name__ == "__main__":
+    unittest.main()
