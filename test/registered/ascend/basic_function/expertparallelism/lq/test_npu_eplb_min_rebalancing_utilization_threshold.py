dce39cfb57ca7dc8617212d9a27cf4e611740be7
YZY00Raiser <2452658175@qq.com>
2026-04-02 15:00:47 +0800

    update

--- test/registered/ascend/basic_function/expertparallelism/lq/test_npu_eplb_min_rebalancing_utilization_threshold.py
+++ test/registered/ascend/basic_function/expertparallelism/lq/test_npu_eplb_min_rebalancing_utilization_threshold.py
@@ -126,7 +126,6 @@ class TestEplbMinRebalancingUtilizationThreshold005(TestEplbMinRebalancingUtiliz
     test_args = ["--eplb-min-rebalancing-utilization-threshold", 0.05]
 
 
-@unittest.skip("Temporarily skipped due to execution failure. Issue #49 has been filed for investigation.")
 class TestEplbMinRebalancingUtilizationThreshold095(TestEplbMinRebalancingUtilizationThresholdBase, CustomTestCase):
     """
     Testcase：When the configuration --eplb-min-rebalancing-utilization-threshold is set to 0.95, if load balancing
