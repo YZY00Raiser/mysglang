77c9f44c6f364c2da9378f10f1c459f5bf862ee7
YZY00Raiser <2452658175@qq.com>
2026-04-02 17:39:48 +0800

    update

---
+++ test/registered/ascend/basic_function/ConfigurationFileSupport/pr/ConfigurationFileSupport/_test_npu_config.py
@@ -0,0 +1,251 @@
+import tempfile
+import os
+import subprocess
+import unittest
+
+import requests
+
+from sglang.srt.utils import kill_process_tree
+from sglang.test.ascend.test_ascend_utils import (
+    CONFIG_YAML_PATH,
+    popen_launch_server_with_config_yaml,
+)
+from sglang.test.ci.ci_register import register_npu_ci
+from sglang.test.test_utils import (
+    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+    DEFAULT_URL_FOR_TEST,
+    CustomTestCase,
+    popen_launch_server, _wait_for_server_health, _create_clean_subprocess_env,
+)
+
+register_npu_ci(
+    est_time=400,
+    suite="nightly-4-npu-a3",
+    nightly=True,
+)
+
+
+# CONFIG_YAML_PATH = (
+#     "/data/y30082119/mysglang/test/registered/ascend/basic_function/ConfigurationFileSupport/config.yaml"
+# )
+
+
+class TestSGLangConfigServer(CustomTestCase):
+    config = CONFIG_YAML_PATH
+
+    @classmethod
+    def launch_server_with_config_yaml(cls, config_file, base_url, timeout):
+        _, host, port = base_url.split(":")
+        host = host[2:]
+        command = [
+            "python3",
+            "-m",
+            "sglang.launch_server",
+            "--config", config_file,
+            "--host", host,
+            "--port", port,
+        ]
+
+        env = _create_clean_subprocess_env(os.environ.copy())
+        process = subprocess.Popen(
+            command,
+            stdout=None,
+            stderr=None,
+            env=env
+        )
+        _wait_for_server_health(process, base_url, None, timeout)
+        return process
+
+    @classmethod
+    def setUpClass(cls):
+        cls.process = cls.launch_server_with_config_yaml(
+            cls.config,
+            DEFAULT_URL_FOR_TEST,
+            DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_config_yaml_server_generate(self):
+        """测试 yaml 配置启动的服务是否正常生成文本"""
+        response = requests.post(
+            f"{DEFAULT_URL_FOR_TEST}/generate",
+            json={
+                "text": "The capital of France is",
+                "sampling_params": {
+                    "temperature": 0,
+                    "max_new_tokens": 32,
+                },
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Paris", response.text)
+
+
+'''
+class TestConfig(CustomTestCase):
+    """Testcase: Verify set --config parameter, can identify the set config and inference request is successfully processed.
+
+    [Test Category] Parameter
+    [Test Target] --config
+    """
+
+    config = CONFIG_YAML_PATH
+
+    @classmethod
+    def launch_server_with_config_yaml(cls, config_file, base_url, timeout):
+        _, host, port = base_url.split(":")
+        host = host[2:]
+        command = [
+            "python3",
+            "-m",
+            "sglang.launch_server",
+            "--config", config_file,
+            "--host", host,
+            "--port", port,
+        ]
+
+        env = _create_clean_subprocess_env(os.environ.copy())
+        process = subprocess.Popen(
+            command,
+            stdout=None,
+            stderr=None,
+            env=env
+        )
+        _wait_for_server_health(process, base_url, None, timeout)
+        return process
+
+    @classmethod
+    def setUpClass(cls):
+        cls.process = cls.launch_server_with_config_yaml(
+            cls.config,
+            DEFAULT_URL_FOR_TEST,
+            DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_config_yaml_server_generate(self):
+        response = requests.post(
+            f"{DEFAULT_URL_FOR_TEST}/generate",
+            json={
+                "text": "The capital of France is",
+                "sampling_params": {
+                    "temperature": 0,
+                    "max_new_tokens": 32,
+                },
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Paris", response.text)
+'''
+
+'''
+class TestConfig(CustomTestCase):
+    """Testcase: Verify set --config parameter, can identify the set config and inference request is successfully processed.
+
+    [Test Category] Parameter
+    [Test Target] --config
+    """
+
+    config = CONFIG_YAML_PATH
+
+    @classmethod
+    def launch_server_with_config_yaml(cls, config_file, base_url, timeout):
+        _, host, port = base_url.split(":")
+        host = host[2:]
+        command = [
+            "python3",
+            "-m",
+            "sglang.launch_server",
+            "--config", config_file,
+            "--host", host,
+            "--port", port,
+        ]
+
+        env = _create_clean_subprocess_env(os.environ.copy())
+        process = subprocess.Popen(
+            command,
+            stdout=None,
+            stderr=None,
+            env=env
+        )
+        _wait_for_server_health(process, base_url, None, timeout)
+        return process
+
+    @classmethod
+    def setUpClass(cls):
+        cls.process = popen_launch_server_with_config_yaml(
+            cls.config,
+            DEFAULT_URL_FOR_TEST,
+            DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_config_yaml_server_generate(self):
+        response = requests.post(
+            f"{DEFAULT_URL_FOR_TEST}/generate",
+            json={
+                "text": "The capital of France is",
+                "sampling_params": {
+                    "temperature": 0,
+                    "max_new_tokens": 32,
+                },
+            },
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Paris", response.text)
+'''
+
+'''
+class TestConfigPriority(CustomTestCase):
+    """Testcase: Verify set the parameter set in the command line have a higher priority than set in config.yaml,
+    set false model path in the command, set right model path in the config.yaml,
+    will use false model path service start fail .
+
+    [Test Category] Parameter
+    [Test Target] --config
+    """
+
+    model = "/nonexistent/Qwen/Qwen3-32B"
+    config = CONFIG_YAML_PATH
+
+    def test_config_priority(self):
+        error_message = "Repo id must be in the form 'repo_name' or 'namespace/repo_name': '/nonexistent/Qwen/Qwen3-32B'."
+        with tempfile.NamedTemporaryFile(
+            mode="w+", delete=True, suffix="out.log"
+        ) as out_log_file, tempfile.NamedTemporaryFile(
+            mode="w+", delete=True, suffix="out.log"
+        ) as err_log_file:
+            try:
+                popen_launch_server(
+                    self.model,
+                    DEFAULT_URL_FOR_TEST,
+                    timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+                    other_args=["--config", self.config],
+                    return_stdout_stderr=(out_log_file, err_log_file),
+                )
+            except Exception as e:
+                self.assertIn(
+                    "Server process exited with code 1.",
+                    str(e),
+                )
+            finally:
+                err_log_file.seek(0)
+                content = err_log_file.read()
+                # error_message information is recorded in the error log
+                self.assertIn(error_message, content)
+'''
+
+if __name__ == "__main__":
+    unittest.main()
--- test/registered/ascend/basic_function/ConfigurationFileSupport/pr/ConfigurationFileSupport/test_npu_config.py
+++ test/registered/ascend/basic_function/ConfigurationFileSupport/pr/ConfigurationFileSupport/test_npu_config.py
@@ -15,9 +15,7 @@ from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
     DEFAULT_URL_FOR_TEST,
     CustomTestCase,
-    popen_launch_server,
-    _create_clean_subprocess_env,
-    _wait_for_server_health,
+    popen_launch_server, _create_clean_subprocess_env, _wait_for_server_health,
 )
 
 register_npu_ci(
@@ -38,41 +36,38 @@ class TestConfig(CustomTestCase):
 
     # launch server with "--config" parameter
     @classmethod
-    def popen_launch_server_with_config_yaml(cls, config_file, base_url, timeout):
-        _, host, port = base_url.split(":")
-        host = host[2:]
+    def popen_launch_server_with_config_yaml(cls,config_file, base_url, timeout):
+        parsed_url = urlparse(base_url)
+        host = parsed_url.hostname
+        port = parsed_url.port
         command = [
             "python3",
             "-m",
             "sglang.launch_server",
-            "--config", config_file,
-            "--host", host,
-            "--port", port,
+            "--config",
+            config_file,
+            "--host",
+            host,
+            "--port",
+            port,
         ]
 
         env = _create_clean_subprocess_env(os.environ.copy())
-        process = subprocess.Popen(
-            command,
-            stdout=None,
-            stderr=None,
-            env=env
-        )
+        process = subprocess.Popen(command, stdout=None, stderr=None, env=env)
         _wait_for_server_health(process, base_url, None, timeout)
         return process
 
     @classmethod
     def setUpClass(cls):
         cls.process = cls.popen_launch_server_with_config_yaml(
-            cls.config,
-            DEFAULT_URL_FOR_TEST,
-            DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH
+            cls.config, DEFAULT_URL_FOR_TEST, DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH
         )
 
     @classmethod
     def tearDownClass(cls):
         kill_process_tree(cls.process.pid)
 
-    def test_config_yaml_server_generate(self):
+    def test_config(self):
         response = requests.post(
             f"{DEFAULT_URL_FOR_TEST}/generate",
             json={
