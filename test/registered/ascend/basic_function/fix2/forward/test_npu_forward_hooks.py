f0cc6aae9ac665763a7094c03bee63762671f6c7
YZY00Raiser <2452658175@qq.com>
2026-04-07 09:44:11 +0800

    update

--- test/registered/ascend/basic_function/fix2/forward/test_npu_forward_hooks.py
+++ test/registered/ascend/basic_function/fix2/forward/test_npu_forward_hooks.py
@@ -5,9 +5,9 @@ import unittest
 import requests
 
 from sglang.srt.utils import kill_process_tree
-from sglang.test.ascend.test_ascend_utils import (
-    QWEN3_32B_WEIGHTS_PATH,
-)
+# from sglang.test.ascend.test_ascend_utils import (
+#     QWEN3_32B_WEIGHTS_PATH,
+# )
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import (
     DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
@@ -21,7 +21,7 @@ register_npu_ci(
     suite="nightly-4-npu-a3",
     nightly=True,
 )
-
+QWEN3_32B_WEIGHTS_PATH="/home/weights/Qwen/Qwen3-32B"
 
 class TestSetForwardHooks(CustomTestCase):
     """Testcase: Verify set --forward-hooks parameter, can identify the set hook function
