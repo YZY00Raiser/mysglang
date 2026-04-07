15d16fbfe28a94ba7f5d330e2ac4eb37bbe7a9ee
YZY00Raiser <2452658175@qq.com>
2026-04-07 15:22:08 +0800

    update

--- test/registered/ascend/basic_function/lora/doubt/_test_npu_overlap_loading.py
+++ test/registered/ascend/basic_function/lora/doubt/_test_npu_overlap_loading.py
@@ -46,7 +46,7 @@ class TestLoraOverlapLoadingDisabled(CustomTestCase):
         "ascend",
         "--disable-cuda-graph",
         "--base-gpu-id",
-        "10",
+        "8",
     ]
 
     @classmethod
@@ -123,7 +123,7 @@ class TestLoraOverlapLoadingEnabled(CustomTestCase):
             "--disable-cuda-graph",
             "--enable-lora-overlap-loading",
             "--base-gpu-id",
-            "10",
+            "8",
         ]
         cls.process = popen_launch_server(
             LLAMA_3_2_1B_INSTRUCT_WEIGHTS_PATH,
