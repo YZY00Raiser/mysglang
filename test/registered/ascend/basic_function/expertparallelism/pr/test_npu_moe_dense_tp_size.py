d4107871798f7741fe3d31ca0c504163b242d77f
YZY00Raiser <2452658175@qq.com>
2026-04-08 15:58:34 +0800

    update

--- test/registered/ascend/basic_function/expertparallelism/pr/test_npu_moe_dense_tp_size.py
+++ test/registered/ascend/basic_function/expertparallelism/pr/test_npu_moe_dense_tp_size.py
@@ -3,7 +3,7 @@ from types import SimpleNamespace
 from urllib.parse import urlparse
 
 from sglang.srt.utils import kill_process_tree
-from sglang.test.ascend.test_ascend_utils import DEEPSEEK_CODER_V2_LITE_WEIGHTS_PATH
+# from sglang.test.ascend.test_ascend_utils import DEEPSEEK_CODER_V2_LITE_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.few_shot_gsm8k import run_eval as run_eval_few_shot_gsm8k
 from sglang.test.test_utils import (
@@ -14,6 +14,8 @@ from sglang.test.test_utils import (
 )
 
 register_npu_ci(est_time=400, suite="nightly-2-npu-a3", nightly=True)
+
+DEEPSEEK_CODER_V2_LITE_WEIGHTS_PATH = "/home/weights/DeepSeek-Coder-V2-Lite-Instruct"
 
 
 class TestAscendMoeDenseTPSize(CustomTestCase):
