fef931691e140ed9cf79ea20d545ebd5dd651f82
YZY00Raiser <2452658175@qq.com>
2026-04-08 11:50:20 +0800

    update

--- test/registered/ascend/basic_function/ForCheckpointDecryption/test_npu_decrypted_draft_config_file.py
+++ test/registered/ascend/basic_function/ForCheckpointDecryption/test_npu_decrypted_draft_config_file.py
@@ -8,8 +8,8 @@ import requests
 from sglang.srt.utils import kill_process_tree
 from sglang.test.ascend.test_ascend_utils import run_command
 # from sglang.test.ascend.test_ascend_utils import (
-#     QWEN3_32B_WEIGHTS_PATH,
-#     QWEN3_32B_EAGLE3_WEIGHTS_PATH
+#     QWEN3_8B_WEIGHTS_PATH,
+#     QWEN3_8B_EAGLE3_WEIGHTS_PATH
 # )
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.test_utils import (
@@ -40,20 +40,20 @@ class TestSetForwardHooks(CustomTestCase):
 
     @classmethod
     def setUpClass(cls):
-        run_command(
-            f"mv {os.path.join(QWEN3_8B_WEIGHTS_PATH, 'config.json')} {os.path.join(QWEN3_8B_WEIGHTS_PATH, '_config.json')}")
-        run_command(
-            f"mv {os.path.join(QWEN3_8B_EAGLE3_WEIGHTS_PATH, 'config.json')} {os.path.join(QWEN3_8B_EAGLE3_WEIGHTS_PATH, '_config.json')}")
+        # run_command(
+        #     f"mv {os.path.join(QWEN3_8B_WEIGHTS_PATH, 'config.json')} {os.path.join(QWEN3_8B_WEIGHTS_PATH, '_config.json')}")
+        # run_command(
+        #     f"mv {os.path.join(QWEN3_8B_EAGLE3_WEIGHTS_PATH, 'config.json')} {os.path.join(QWEN3_8B_EAGLE3_WEIGHTS_PATH, '_config.json')}")
 
-        cls.extra_envs = {
-            "HCCL_BUFFSIZE": "1024",
-            "SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK": "32",
-            "SGLANG_NPU_USE_MLAPO": "1",
-            "SGLANG_NPU_USE_EINSUM_MM": "1",
-            "SLANG_ENABLE_SPEC_V2": "1",
-            "SGLANG_ENABLE_OVERLAP_PLAN_STREAM": "1",
-        }
-        os.environ.update(cls.extra_envs)
+        # cls.extra_envs = {
+        #     "HCCL_BUFFSIZE": "1024",
+        #     "SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK": "32",
+        #     "SGLANG_NPU_USE_MLAPO": "1",
+        #     "SGLANG_NPU_USE_EINSUM_MM": "1",
+        #     "SLANG_ENABLE_SPEC_V2": "1",
+        #     "SGLANG_ENABLE_OVERLAP_PLAN_STREAM": "1",
+        # }
+        # os.environ.update(cls.extra_envs)
 
         # Service failed to start, restoring original file name
         try:
@@ -115,86 +115,6 @@ class TestSetForwardHooks(CustomTestCase):
                     print(f"Warning: Neither {old_path} nor {new_path} exists")
             if cls.process:
                 kill_process_tree(cls.process.pid)
-
-        '''
-        try:
-            # launch server with "--config" parameter
-            parsed_url = urlparse(DEFAULT_URL_FOR_TEST)
-            host = parsed_url.hostname
-            port = str(parsed_url.port)
-            command = [
-                "python3",
-                "-m",
-                "sglang.launch_server",
-                "--model-path",
-                QWEN3_32B_WEIGHTS_PATH,
-                "--host",
-                host,
-                "--port",
-                port,
-                "--trust-remote-code",
-                "--attention-backend",
-                "ascend",
-                "--disable-radix-cache",
-                "--chunked-prefill-size",
-                "-1",
-                "--max-prefill-tokens",
-                "1024",
-                "--speculative-algorithm",
-                "EAGLE3",
-                "--speculative-draft-model-path",
-                QWEN3_32B_EAGLE3_WEIGHTS_PATH,
-                "--speculative-num-steps",
-                "3",
-                "--speculative-eagle-topk",
-                "1",
-                "--speculative-num-draft-tokens",
-                "4",
-                "--tp-size",
-                "2",
-                "--mem-fraction-static",
-                "0.68",
-                "--disable-cuda-graph",
-                "--dtype",
-                "bfloat16",
-                "--decrypted-config-file",
-                "/home/y30082119/Qwen3-8B/config.json",
-                "--decrypted-draft-config-file",
-                "/home/y30082119/Qwen3-8B_eagle3/config.json",
-                "--base-gpu-id",
-                "12",
-            ]
-
-            env = _create_clean_subprocess_env(os.environ.copy())
-            cls.extra_envs = {
-                "SLANG_ENABLE_SPEC_V2": "1",
-                "SGLANG_ENABLE_OVERLAP_PLAN_STREAM": "1",
-            }
-
-            os.environ.update(cls.extra_envs)
-            cls.process = subprocess.Popen(command, stdout=None, stderr=None, env=env)
-            _wait_for_server_health(
-                cls.process, DEFAULT_URL_FOR_TEST, None, DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH
-            )
-
-        except Exception as e:
-            raise RuntimeError(f"Failed to launch server: {e}") from e
-
-        finally:
-            for weights_path in [QWEN3_32B_WEIGHTS_PATH, QWEN3_32B_EAGLE3_WEIGHTS_PATH]:
-                old_path = os.path.join(weights_path, '_config.json')
-                new_path = os.path.join(weights_path, 'config.json')
-
-                if os.path.exists(old_path):
-                    try:
-                        run_command(f"mv {old_path} {new_path}")
-                    except Exception as e:
-                        print(f"Warning: Failed to rename {old_path}: {e}")
-                elif not os.path.exists(new_path):
-                    print(f"Warning: Neither {old_path} nor {new_path} exists")
-            if cls.process:
-                kill_process_tree(cls.process.pid)
-    '''
 
     '''
     @classmethod
