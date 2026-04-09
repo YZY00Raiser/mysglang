778a5afeca266bb6c443ae6a75becfccd4179ec8
YZY00Raiser <2452658175@qq.com>
2026-04-09 19:56:57 +0800

    update

--- test/registered/ascend/basic_function/expertparallelism/pr/test_npu_eplb_min_rebalancing_utilization_threshold.py
+++ test/registered/ascend/basic_function/expertparallelism/pr/test_npu_eplb_min_rebalancing_utilization_threshold.py
@@ -108,6 +108,7 @@ class TestEplbMinRebalancingUtilizationThresholdBase(CustomTestCase):
             num_examples=200,
             num_threads=128,
             num_shots=5,
+            data_path="/home/y30082119/test.jsonl",
         )
         metrics = run_eval(args)
         self.assertGreaterEqual(
