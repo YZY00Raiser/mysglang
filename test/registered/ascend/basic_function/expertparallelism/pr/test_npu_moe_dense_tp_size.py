105924f41c409895a67662eecc30e656096a5c31
YZY00Raiser <2452658175@qq.com>
2026-04-09 20:34:53 +0800

    update

--- test/registered/ascend/basic_function/expertparallelism/pr/test_npu_moe_dense_tp_size.py
+++ test/registered/ascend/basic_function/expertparallelism/pr/test_npu_moe_dense_tp_size.py
@@ -73,7 +73,7 @@ class TestAscendMoeDenseTPSize(CustomTestCase):
             num_shots=5,
         )
         metrics = run_eval(args)
-        self.assertGreater(metrics["score"], 0.79)
+        self.assertGreater(metrics["score"], 0.81)
 
 
 if __name__ == "__main__":
