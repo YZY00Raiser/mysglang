3ad957ba8e0db2c807cc4e9ee0153055a3c162ce
YZY00Raiser <2452658175@qq.com>
2026-04-10 13:55:27 +0800

    update

--- test/registered/ascend/basic_function/ForCheckpointDecryption/test_npu_decrypted_draft_config_file.py
+++ test/registered/ascend/basic_function/ForCheckpointDecryption/test_npu_decrypted_draft_config_file.py
@@ -79,7 +79,7 @@ class TestSetForwardHooks(CustomTestCase):
                     "--decrypted-draft-config-file",
                     "/home/y30082119/Qwen3-8B_eagle3/config.json",
                     "--base-gpu-id",
-                    "12",
+                    "4",
                 ],
                 env={
                     "SGLANG_ENABLE_SPEC_V2": "1",
