8515e7c0c2e22e418c04e1a36c7b13ea56539435
YZY00Raiser <2452658175@qq.com>
2026-04-10 11:17:52 +0800

    update

--- test/registered/ascend/basic_function/expertparallelism/pr/test_npu_expert_distribution_recorder_mode.py
+++ test/registered/ascend/basic_function/expertparallelism/pr/test_npu_expert_distribution_recorder_mode.py
@@ -60,7 +60,7 @@ class TestExpertDistributionRecorderModeStatic(CustomTestCase):
                 "--expert-distribution-recorder-mode",
                 cls.expert_distribution_recorder_mode,
                 "--base-gpu-id",
-                "2",
+                "4",
             ],
             env={
                 # "SGLANG_NPU_DISABLE_ACL_FORMAT_WEIGHT": "1",
