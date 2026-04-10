44d0e22270d02ca8f7ad68caacdc74d4a2605767
YZY00Raiser <2452658175@qq.com>
2026-04-10 14:51:25 +0800

    update

--- test/registered/ascend/basic_function/ForCheckpointDecryption/test_npu_decrypted_draft_config_file.py
+++ test/registered/ascend/basic_function/ForCheckpointDecryption/test_npu_decrypted_draft_config_file.py
@@ -27,6 +27,7 @@ register_npu_ci(
 QWEN3_8B_WEIGHTS_PATH = "/home/weights/Qwen/Qwen3-8B"
 QWEN3_8B_EAGLE3_WEIGHTS_PATH = "/home/weights/Qwen/Qwen3-8B_eagle3"
 
+
 class TestDraftConfigFile(CustomTestCase):
     """Testcase: Verify set --decrypted-config-file, --decrypted-draft-config-file parameter,
     will use the specified config.json and the GSM8K dataset is no less than 0.95.
@@ -44,62 +45,63 @@ class TestDraftConfigFile(CustomTestCase):
         run_command(
             f"mv {os.path.join(QWEN3_8B_EAGLE3_WEIGHTS_PATH, 'config.json')} {os.path.join(QWEN3_8B_EAGLE3_WEIGHTS_PATH, '_config.json')}"
         )
-        # try:
-        cls.model=QWEN3_8B_WEIGHTS_PATH
-        cls.process = popen_launch_server(
-            cls.model,
-            DEFAULT_URL_FOR_TEST,
-            DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=[
-                "--trust-remote-code",
-                "--attention-backend",
-                "ascend",
-                "--disable-radix-cache",
-                "--chunked-prefill-size",
-                "-1",
-                "--max-prefill-tokens",
-                "1024",
-                "--speculative-algorithm",
-                "EAGLE3",
-                "--speculative-draft-model-path",
-                QWEN3_8B_EAGLE3_WEIGHTS_PATH,
-                "--speculative-num-steps",
-                "3",
-                "--speculative-eagle-topk",
-                "1",
-                "--speculative-num-draft-tokens",
-                "4",
-                "--tp-size",
-                "2",
-                "--mem-fraction-static",
-                "0.68",
-                "--disable-cuda-graph",
-                "--dtype",
-                "bfloat16",
-                "--decrypted-config-file",
-                "/home/y30082119/Qwen3-8B/config.json",
-                "--decrypted-draft-config-file",
-                "/home/y30082119/Qwen3-8B_eagle3/config.json",
-                "--base-gpu-id",
-                "2",
-            ],
-            env={
-                "SGLANG_ENABLE_SPEC_V2": "1",
-                "SGLANG_ENABLE_OVERLAP_PLAN_STREAM": "1",
-            },
-        )
-        # except Exception as e:
-        #     raise RuntimeError(f"Failed to launch server: {e}") from e
-        # finally:
-        #     # Service failed to start, restoring original file name
-        #     run_command(
-        #         f"mv {os.path.join(QWEN3_8B_WEIGHTS_PATH, '_config.json')} {os.path.join(QWEN3_8B_WEIGHTS_PATH, 'config.json')}"
-        #     )
-        #     run_command(
-        #         f"mv {os.path.join(QWEN3_8B_EAGLE3_WEIGHTS_PATH, '_config.json')} {os.path.join(QWEN3_8B_EAGLE3_WEIGHTS_PATH, 'config.json')}"
-        #     )
-        #     if cls.process:
-        #         kill_process_tree(cls.process.pid)
+        try:
+            cls.model = QWEN3_8B_WEIGHTS_PATH
+            cls.process = popen_launch_server(
+                cls.model,
+                DEFAULT_URL_FOR_TEST,
+                DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+                other_args=[
+                    "--trust-remote-code",
+                    "--attention-backend",
+                    "ascend",
+                    "--disable-radix-cache",
+                    "--chunked-prefill-size",
+                    "-1",
+                    "--max-prefill-tokens",
+                    "1024",
+                    "--speculative-algorithm",
+                    "EAGLE3",
+                    "--speculative-draft-model-path",
+                    QWEN3_8B_EAGLE3_WEIGHTS_PATH,
+                    "--speculative-num-steps",
+                    "3",
+                    "--speculative-eagle-topk",
+                    "1",
+                    "--speculative-num-draft-tokens",
+                    "4",
+                    "--tp-size",
+                    "2",
+                    "--mem-fraction-static",
+                    "0.68",
+                    "--disable-cuda-graph",
+                    "--dtype",
+                    "bfloat16",
+                    "--decrypted-config-file",
+                    "/home/y30082119/Qwen3-8B/config.json",
+                    "--decrypted-draft-config-file",
+                    "/home/y30082119/Qwen3-8B_eagle3/config.json",
+                    "--base-gpu-id",
+                    "2",
+                ],
+                env={
+                    "SGLANG_ENABLE_SPEC_V2": "1",
+                    "SGLANG_ENABLE_OVERLAP_PLAN_STREAM": "1",
+                },
+            )
+
+        except Exception as e:
+            raise RuntimeError(f"Failed to launch server: {e}") from e
+        finally:
+            # Service failed to start, restoring original file name
+            run_command(
+                f"mv {os.path.join(QWEN3_8B_WEIGHTS_PATH, '_config.json')} {os.path.join(QWEN3_8B_WEIGHTS_PATH, 'config.json')}"
+            )
+            run_command(
+                f"mv {os.path.join(QWEN3_8B_EAGLE3_WEIGHTS_PATH, '_config.json')} {os.path.join(QWEN3_8B_EAGLE3_WEIGHTS_PATH, 'config.json')}"
+            )
+            if cls.process:
+                kill_process_tree(cls.process.pid)
 
     @classmethod
     def tearDownClass(cls):
@@ -110,11 +112,6 @@ class TestDraftConfigFile(CustomTestCase):
             f"mv {os.path.join(QWEN3_8B_EAGLE3_WEIGHTS_PATH, '_config.json')} {os.path.join(QWEN3_8B_EAGLE3_WEIGHTS_PATH, 'config.json')}"
         )
         kill_process_tree(cls.process.pid)
-
-    # def testprint(self):
-    #     print("sssssssssssssssssssssssssssssssssssssssssss")
-
-
 
     def test(self):
         response = requests.post(
@@ -133,6 +130,7 @@ class TestDraftConfigFile(CustomTestCase):
         self.assertIn(
             "Paris", response.text, "The inference result does not include Paris."
         )
+
     '''
     def test_gsm8k(self):
         args = SimpleNamespace(
