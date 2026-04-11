c6e725f05d5ad29661b3aa9d4835375165423918
YZY00Raiser <2452658175@qq.com>
2026-04-11 11:30:45 +0800

    update

--- test/registered/ascend/basic_function/expertparallelism/ci/test_dispatch_w8a8.py
+++ test/registered/ascend/basic_function/expertparallelism/ci/test_dispatch_w8a8.py
@@ -8,6 +8,8 @@ import requests
 from sglang.srt.utils import kill_process_tree
 from sglang.test.ascend.test_ascend_utils import run_command
 # from sglang.test.ascend.test_ascend_utils import DEEPSEEK_CODER_V2_LITE_WEIGHTS_PATH
+from sglang.test.ascend.test_ascend_utils import DEEPSEEK_V3_2_W8A8_WEIGHTS_PATH
+
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.run_eval import run_eval
 from sglang.test.test_utils import (
@@ -18,7 +20,7 @@ from sglang.test.test_utils import (
 )
 
 register_npu_ci(est_time=400, suite="nightly-2-npu-a3", nightly=True)
-DEEPSEEK_CODER_V2_LITE_WEIGHTS_PATH = "/home/weights/DeepSeek-Coder-V2-Lite-Instruct"
+# DEEPSEEK_CODER_V2_LITE_WEIGHTS_PATH = "/home/weights/DeepSeek-Coder-V2-Lite-Instruct"
 
 
 class TestExpertDistributionRecorderModeStatic(CustomTestCase):
@@ -36,7 +38,7 @@ class TestExpertDistributionRecorderModeStatic(CustomTestCase):
     @classmethod
     def setUpClass(cls):
         cls.process = popen_launch_server(
-            DEEPSEEK_CODER_V2_LITE_WEIGHTS_PATH,
+            DEEPSEEK_V3_2_W8A8_WEIGHTS_PATH,
             DEFAULT_URL_FOR_TEST,
             DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
             other_args=[
