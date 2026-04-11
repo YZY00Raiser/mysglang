c713feac0a1f6136d50f18470ef28a25051f1f3b
YZY00Raiser <2452658175@qq.com>
2026-04-11 14:27:16 +0800

    update

--- test/registered/ascend/basic_function/expertparallelism/ci/test_dispatch_w8a8.py
+++ test/registered/ascend/basic_function/expertparallelism/ci/test_dispatch_w8a8.py
@@ -5,6 +5,7 @@ import requests
 
 from sglang.srt.utils import kill_process_tree
 from sglang.test.ascend.test_ascend_utils import DEEPSEEK_V3_2_W8A8_WEIGHTS_PATH
+from sglang.test.ascend.test_ascend_utils import DEEPSEEK_CODER_V2_LITE_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.run_eval import run_eval
 from sglang.test.test_utils import (
