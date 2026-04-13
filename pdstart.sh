69d40d63e821aaf4d9f4b2a11dbec1f035c77fd4
YZY00Raiser <2452658175@qq.com>
2026-04-13 15:07:16 +0800

    update

--- pdstart.sh
+++ pdstart.sh
@@ -1,7 +1,12 @@
+# 必须和 Prefill/Decode 节点的 IP 保持一致
+export PREFILL_HOST_IP=172.22.3.154
+export DECODE_HOST_IP=172.22.3.71
+
+# 启动 SGLang 路由网关（自动转发 Prefill + Decode 请求）
 python -m sglang_router.launch_router \
     --pd-disaggregation \
     --policy cache_aware \
-    --prefill http://<PREFILL_HOST_IP>:8000 8996 \
-    --decode http://<DECODE_HOST_IP>:8001 \
-    --host 127.0.0.1 \
+    --prefill http://${PREFILL_HOST_IP}:8000 8996 \
+    --decode http://${DECODE_HOST_IP}:8001 \
+    --host 0.0.0.0 \
     --port 6688
