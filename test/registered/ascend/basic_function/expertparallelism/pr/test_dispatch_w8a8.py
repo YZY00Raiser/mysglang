27b2fb0983209511136ee99ad6123f5c22de6125
YZY00Raiser <2452658175@qq.com>
2026-04-13 17:43:54 +0800

    update

--- test/registered/ascend/basic_function/expertparallelism/pr/test_dispatch_w8a8.py
+++ test/registered/ascend/basic_function/expertparallelism/pr/test_dispatch_w8a8.py
@@ -1,12 +1,10 @@
-import glob
-import os
 import unittest
 from types import SimpleNamespace
 
 import requests
 
 from sglang.srt.utils import kill_process_tree
-from sglang.test.ascend.test_ascend_utils import run_command
+# from sglang.test.ascend.test_ascend_utils import DEEPSEEK_V3_2_W8A8_WEIGHTS_PATH
 # from sglang.test.ascend.test_ascend_utils import DEEPSEEK_CODER_V2_LITE_WEIGHTS_PATH
 from sglang.test.ci.ci_register import register_npu_ci
 from sglang.test.run_eval import run_eval
@@ -18,25 +16,25 @@ from sglang.test.test_utils import (
 )
 
 register_npu_ci(est_time=400, suite="nightly-2-npu-a3", nightly=True)
-DEEPSEEK_CODER_V2_LITE_WEIGHTS_PATH = "/home/weights/DeepSeek-Coder-V2-Lite-Instruct"
 
 
-class TestExpertDistributionRecorderModeStatic(CustomTestCase):
+DEEPSEEK_V3_2_W8A8_WEIGHTS_PATH = "/home/weights/DeepSeek-V3.2-W8A8"
+
+
+class TestEPLBDispatchAlgorithmStatic(CustomTestCase):
     """Testcase: Verify that the model accuracy remains uncompromised when the parameter --moe-dense-tp-size is configured to 1.
 
     [Test Category] Parameter
-    [Test Target] --expert-distribution-recorder-mode
+    [Test Target] --ep-dispatch-algorithm, --moe-a2a-backend
     """
 
-    # expert_distribution_recorder_mode = "per_token"
-    expert_distribution_recorder_mode = "stat"
-
-    path = "/tmp/pt"
+    ep_dispatch_algorithm = "static"
+    model = DEEPSEEK_V3_2_W8A8_WEIGHTS_PATH
 
     @classmethod
     def setUpClass(cls):
         cls.process = popen_launch_server(
-            DEEPSEEK_CODER_V2_LITE_WEIGHTS_PATH,
+            cls.model,
             DEFAULT_URL_FOR_TEST,
             DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
             other_args=[
@@ -45,40 +43,53 @@ class TestExpertDistributionRecorderModeStatic(CustomTestCase):
                 "ascend",
                 "--disable-cuda-graph",
                 "--mem-fraction-static",
-                "0.5",
+                "0.8",
                 "--tp-size",
-                "2",
+                "16",
                 "--expert-parallel-size",
-                "2",
-                "--enable-eplb",
+                "16",
+                # "--enable-eplb",
                 "--moe-a2a-backend",
-                "deepep",
-                "--deepep-mode",
-                "normal",
-                "--ep-num-redundant-experts",
-                "4",
-                "--expert-distribution-recorder-mode",
-                cls.expert_distribution_recorder_mode,
-                "--base-gpu-id",
-                "4",
+                "ascend_fuseep",
+                # "--moe-a2a-backend",
+                # "deepep",
+                # "--deepep-mode",
+                # "normal",
+                # "--ep-num-redundant-experts",
+                # "16",
+                "--ep-dispatch-algorithm",
+                cls.ep_dispatch_algorithm,
+                "--quantization",
+                "modelslim",
             ],
             env={
-                # "SGLANG_NPU_DISABLE_ACL_FORMAT_WEIGHT": "1",
+                "SGLANG_NPU_DISABLE_ACL_FORMAT_WEIGHT": "1",
                 "HCCL_BUFFSIZE": "1024",
-                "SGLANG_EXPERT_DISTRIBUTION_RECORDER_DIR": f"{cls.path}",
+                "SGLANG_NPU_FUSED_MOE_MODE": "2",
             },
         )
 
     @classmethod
     def tearDownClass(cls):
         kill_process_tree(cls.process.pid)
-        # run_command(f"rm -rf {cls.path}")
+
+    '''
+    def test_gsm8k(self):
+        args = SimpleNamespace(
+            max_new_tokens=512,
+            base_url=DEFAULT_URL_FOR_TEST,
+            model=DEEPSEEK_V3_2_W8A8_WEIGHTS_PATH,
+            eval_name="gsm8k",
+            api="completion",
+            num_examples=200,
+            num_threads=128,
+            num_shots=5,
+        )
+        metrics = run_eval(args)
+        self.assertGreater(metrics["score"], 0.79)
+    '''
 
     def test_recorder_mode(self):
-        # Start recording
-        requests.post(f"{DEFAULT_URL_FOR_TEST}/start_expert_distribution_record")
-
-
         response = requests.post(
             f"{DEFAULT_URL_FOR_TEST}/generate",
             json={
@@ -96,54 +107,17 @@ class TestExpertDistributionRecorderModeStatic(CustomTestCase):
             "Paris", response.text, "The inference result does not include Paris."
         )
 
-        '''
-        args = SimpleNamespace(
-            max_new_tokens=512,
-            base_url=DEFAULT_URL_FOR_TEST,
-            model=DEEPSEEK_CODER_V2_LITE_WEIGHTS_PATH,
-            eval_name="gsm8k",
-            api="completion",
-            num_examples=200,
-            num_threads=128,
-            num_shots=5,
-        )
-        metrics = run_eval(args)
-        self.assertGreater(metrics["score"], 0.79)
-        '''
-
-        # Stop recording
-        # requests.post(f"{DEFAULT_URL_FOR_TEST}/stop_expert_distribution_record")
-
-        # Export the .pt file
-        requests.post(f"{DEFAULT_URL_FOR_TEST}/dump_expert_distribution_record")
-
-        # Check distribution_recorder_files
-        distribution_recorder_suffixes = "*.pt"
-        distribution_recorder_files = []
-        for suffix in distribution_recorder_suffixes:
-            distribution_recorder_files.extend(
-                glob.glob(os.path.join(self.path, "**", suffix), recursive=True)
-            )
-        self.assertGreater(
-            len(distribution_recorder_files),
-            0,
-            msg=f"No distribution recorder",
-        )
 
 '''
 
 
-class TestExpertDistributionRecorderModeStatApprox(TestExpertDistributionRecorderModeStatic):
-    expert_distribution_recorder_mode = "stat_approx"
+class TestEPLBDispatchAlgorithmDynamic(TestEPLBDispatchAlgorithmStatic):
+    ep_dispatch_algorithm = "dynamic"
 
 
+class TestEPLBDispatchAlgorithmFake(TestEPLBDispatchAlgorithmStatic):
+    ep_dispatch_algorithm = "fake"
+'''
 
-class TestExpertDistributionRecorderPerPass(TestExpertDistributionRecorderModeStatic):
-    expert_distribution_recorder_mode = "per_pass"
-'''
-'''
-class TestExpertDistributionRecorderPerToken(TestExpertDistributionRecorderModeStatic):
-    expert_distribution_recorder_mode = "per_token"
-'''
 if __name__ == "__main__":
     unittest.main()
