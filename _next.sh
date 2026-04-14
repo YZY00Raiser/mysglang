b31b1bcb112364c46da18ddaaa263f40b4ef4457
YZY00Raiser <2452658175@qq.com>
2026-04-14 13:34:14 +0800

    update

--- _next.sh
+++ _next.sh
@@ -47,7 +47,7 @@ export SGLANG_NPU_USE_MULTI_STREAM=0  # dp conflict
 # export ENABLE_ASCEND_MOE_NZ=1
 export ENABLE_PROFILING=0
 
-MODEL_PATH=/home/weights/Qwen3-Next-80B-A3B-Instruct-W8A8
+MODEL_PATH=/home/weights/Qwen/Qwen3-Next-80B-A3B-Instruct-W8A8
 # MODEL_PATH=/home/weights/Qwen3-Coder-Next_W8A8
 # MODEL_PATH=/home/weights/Qwen3-Coder-Next
 # MODEL_PATH=/home/weights/Qwen3-Next-80B-A3B-Instruct
@@ -88,6 +88,6 @@ python3 -m sglang.launch_server --model-path ${MODEL_PATH} \--page-size 128 \
     --cuda-graph-bs 2 4 16 32 48 64 80 96 128 140 156 \
     --mamba-ssm-dtype bfloat16 \
     --base-gpu-id 0 \
-    --speculative-draft-model-path /home/weights/Qwen3-Next-80B-A3B-Instruct \
+    --speculative-draft-model-path /home/weights/Qwen/Qwen3-Next-80B-A3B-Instruct \
     --quantization modelslim \
     --moe-a2a-backend deepep --deepep-mode auto \
