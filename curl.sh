c32c6cf620e85fb16e8df2d1227fd882f379f11a
YZY00Raiser <2452658175@qq.com>
2026-04-14 13:12:19 +0800

    update

--- curl.sh
+++ curl.sh
@@ -1,4 +1,4 @@
-curl --location 'http://172.22.3.19:6688/generate' \
+curl --location 'http://127.0.0.1:30000/generate' \
 --header 'Content-Type:application/json' \
 --data '{"text":"The capital of France is", "sampling_params":{"temperature":0, "max_new_tokens":100}}'
 
