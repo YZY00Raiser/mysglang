54864f8b1e89cc20341e1c240f02db1844a07e63
YZY00Raiser <2452658175@qq.com>
2026-04-15 22:17:37 +0800

    update

--- test/registered/ascend/basic_function/offloading/test_npu_offload_group_size.py
+++ test/registered/ascend/basic_function/offloading/test_npu_offload_group_size.py
@@ -145,6 +145,69 @@ class TestOffload1(CustomTestCase):
 '''
 
 
+class TestOffload1(CustomTestCase):
+    """Testcase: Tests core functionality with --cpu-offload-gb configuration, inference requests successful.
+    and the ingerence accuracy using the GSM8K dataset is no less than 0.86.
+
+    [Test Category] Parameter
+    [Test Target] --offload-mode
+    """
+
+    def test_max_loaded_loras_error(self):
+        other_args = [
+            "--attention-backend",
+            "ascend",
+            "--disable-cuda-graph",
+            "--tp-size",
+            1,
+            "--offload-group-size",
+            "4",
+            "--offload-num-in-group",
+            "2",
+            "--offload-prefetch-step",
+            "2",
+            "--offload-mode",
+            "sharded_gpu",
+            "--base-gpu-id",
+            "14",
+        ]
+
+        out_log_file = open("./cache_out_log.txt", "w+", encoding="utf-8")
+        err_log_file = open("./cache_err_log.txt", "w+", encoding="utf-8")
+        self.process = popen_launch_server(
+            DEEPSEEK_V2_LITE_W8A8_WEIGHTS_PATH,
+            DEFAULT_URL_FOR_TEST,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=other_args,
+            return_stdout_stderr=(out_log_file, err_log_file),
+        )
+        response = requests.post(
+            f"{DEFAULT_URL_FOR_TEST}/generate",
+            json={
+                "text": "The capital of France is",
+                "sampling_params": {
+                    "temperature": 0,
+                    "max_new_tokens": 32,
+                },
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn("France", response.text)
+        print("--------------response.text----------------------")
+        print(response.text)
+        err_log_file.seek(0)
+        content = err_log_file.read()
+        offload_message = "[offloader]"
+        self.assertIn(offload_message, content)
+        out_log_file.close()
+        err_log_file.close()
+        os.remove("./cache_out_log.txt")
+        os.remove("./cache_err_log.txt")
+
+        kill_process_tree(self.process.pid)
+
+
+'''
 
 
 class TestOffload2(CustomTestCase):
@@ -206,6 +269,7 @@ class TestOffload2(CustomTestCase):
         os.remove("./cache_err_log.txt")
 
         kill_process_tree(self.process.pid)
+'''
 
 if __name__ == "__main__":
     unittest.main()
