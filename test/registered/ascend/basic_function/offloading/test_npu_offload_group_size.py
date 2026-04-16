da4d994f542b5faa3b1757b589b4691d812a5a0a
YZY00Raiser <2452658175@qq.com>
2026-04-16 14:22:30 +0800

    update

--- test/registered/ascend/basic_function/offloading/test_npu_offload_group_size.py
+++ test/registered/ascend/basic_function/offloading/test_npu_offload_group_size.py
@@ -172,7 +172,7 @@ class TestOffload1(CustomTestCase):
             "--offload-mode",
             "sharded_gpu",
             "--base-gpu-id",
-            "14",
+            "1",
         ]
 
         out_log_file = open("./cache_out_log.txt", "w+", encoding="utf-8")
