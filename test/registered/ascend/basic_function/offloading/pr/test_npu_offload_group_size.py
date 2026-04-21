ee63c0f441fdd395017959dfb3ac29c90c5e2985
YZY00Raiser <2452658175@qq.com>
2026-04-21 14:12:42 +0800

    update

--- test/registered/ascend/basic_function/offloading/pr/test_npu_offload_group_size.py
+++ test/registered/ascend/basic_function/offloading/pr/test_npu_offload_group_size.py
@@ -14,7 +14,7 @@ from sglang.test.test_utils import (
     popen_launch_server,
 )
 
-register_npu_ci(est_time=400, suite="nightly-1-npu-a3", nightly=True)
+register_npu_ci(est_time=400, suite="nightly-2-npu-a3", nightly=True)
 
 
 class OffloadTestBase(CustomTestCase):
@@ -79,9 +79,13 @@ class TestOffloadGroupSize(OffloadTestBase):
     """
 
     OTHER_ARGS = [
-        "--attention-backend", "ascend",
+        "--attention-backend",
+        "ascend",
         "--disable-cuda-graph",
-        "--offload-group-size", "2",
+        "--offload-group-size",
+        "2",
+        "--tp-size",
+        "2",
     ]
     EXPECT_IN_RESPONSE = True
 
@@ -94,12 +98,17 @@ class TestOffloadMeta(OffloadTestBase):
     """
 
     OTHER_ARGS = [
-        "--attention-backend", "ascend",
+        "--attention-backend",
+        "ascend",
         "--disable-cuda-graph",
-        "--offload-group-size", "4",
-        "--offload-num-in-group", "2",
-        "--offload-prefetch-step", "2",
-        "--offload-mode", "meta",
+        "--offload-group-size",
+        "4",
+        "--offload-num-in-group",
+        "2",
+        "--offload-prefetch-step",
+        "2",
+        "--offload-mode",
+        "meta",
     ]
     # When --offload-mode=meta, it is in debugging mode, creating empty tensors
     # and resulting in incorrect inference results
@@ -117,10 +126,14 @@ class TestOffloadShardedGpu(OffloadTestBase):
         "--attention-backend", "ascend",
         "--disable-cuda-graph",
         "--dp", "2",
-        "--offload-group-size", "4",
-        "--offload-num-in-group", "2",
-        "--offload-prefetch-step", "2",
-        "--offload-mode", "sharded_gpu",
+        "--offload-group-size",
+        "4",
+        "--offload-num-in-group",
+        "2",
+        "--offload-prefetch-step",
+        "2",
+        "--offload-mode",
+        "sharded_gpu",
     ]
     EXPECT_IN_RESPONSE = True
 
