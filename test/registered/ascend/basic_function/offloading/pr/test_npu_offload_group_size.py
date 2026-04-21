61d2a0b206d84663af503f2d467f2a5402b774ff
YZY00Raiser <2452658175@qq.com>
2026-04-21 10:32:22 +0800

    update

--- test/registered/ascend/basic_function/offloading/pr/test_npu_offload_group_size.py
+++ test/registered/ascend/basic_function/offloading/pr/test_npu_offload_group_size.py
@@ -27,8 +27,9 @@ class TestOffloadGroupSize(CustomTestCase):
     [Test Target] --offload-group-size
     """
 
-    def test_max_loaded_loras_error(self):
-        other_args = [
+    @classmethod
+    def setUpClass(cls):
+        cls.other_args = [
             "--attention-backend",
             "ascend",
             "--disable-cuda-graph",
@@ -36,15 +37,25 @@ class TestOffloadGroupSize(CustomTestCase):
             "2",
         ]
 
-        out_log_file = open("./cache_out_log.txt", "w+", encoding="utf-8")
-        err_log_file = open("./cache_err_log.txt", "w+", encoding="utf-8")
-        self.process = popen_launch_server(
+        cls.out_log_file = open("./cache_out_log.txt", "w+", encoding="utf-8")
+        cls.err_log_file = open("./cache_err_log.txt", "w+", encoding="utf-8")
+        cls.process = popen_launch_server(
             DEEPSEEK_V2_LITE_W8A8_WEIGHTS_PATH,
             DEFAULT_URL_FOR_TEST,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=other_args,
-            return_stdout_stderr=(out_log_file, err_log_file),
+            other_args=cls.other_args,
+            return_stdout_stderr=(cls.out_log_file, cls.err_log_file),
         )
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.out_log_file.close()
+        cls.err_log_file.close()
+        os.remove("./cache_out_log.txt")
+        os.remove("./cache_err_log.txt")
+        kill_process_tree(cls.process.pid)
+
+    def test_max_loaded_loras_error(self):
         response = requests.post(
             f"{DEFAULT_URL_FOR_TEST}/generate",
             json={
@@ -58,16 +69,10 @@ class TestOffloadGroupSize(CustomTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn("Paris", response.text)
 
-        err_log_file.seek(0)
-        content = err_log_file.read()
+        self.err_log_file.seek(0)
+        content = self.err_log_file.read()
         offload_message = "[offloader]"
         self.assertIn(offload_message, content)
-        out_log_file.close()
-        err_log_file.close()
-        os.remove("./cache_out_log.txt")
-        os.remove("./cache_err_log.txt")
-
-        kill_process_tree(self.process.pid)
 
 
 class TestOffload1(CustomTestCase):
@@ -78,8 +83,9 @@ class TestOffload1(CustomTestCase):
     [Test Target] --offload-mode
     """
 
-    def test_max_loaded_loras_error(self):
-        other_args = [
+    @classmethod
+    def setUpClass(cls):
+        cls.other_args = [
             "--attention-backend",
             "ascend",
             "--disable-cuda-graph",
@@ -93,15 +99,25 @@ class TestOffload1(CustomTestCase):
             "meta",
         ]
 
-        out_log_file = open("./cache_out_log.txt", "w+", encoding="utf-8")
-        err_log_file = open("./cache_err_log.txt", "w+", encoding="utf-8")
-        self.process = popen_launch_server(
+        cls.out_log_file = open("./cache_out_log.txt", "w+", encoding="utf-8")
+        cls.err_log_file = open("./cache_err_log.txt", "w+", encoding="utf-8")
+        cls.process = popen_launch_server(
             DEEPSEEK_V2_LITE_W8A8_WEIGHTS_PATH,
             DEFAULT_URL_FOR_TEST,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=other_args,
-            return_stdout_stderr=(out_log_file, err_log_file),
+            other_args=cls.other_args,
+            return_stdout_stderr=(cls.out_log_file, cls.err_log_file),
         )
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.out_log_file.close()
+        cls.err_log_file.close()
+        os.remove("./cache_out_log.txt")
+        os.remove("./cache_err_log.txt")
+        kill_process_tree(cls.process.pid)
+
+    def test_max_loaded_loras_error(self):
         response = requests.post(
             f"{DEFAULT_URL_FOR_TEST}/generate",
             json={
@@ -114,19 +130,13 @@ class TestOffload1(CustomTestCase):
         )
         self.assertEqual(response.status_code, 200)
         self.assertNotIn("Paris", response.text)
-        err_log_file.seek(0)
-        content = err_log_file.read()
+        self.err_log_file.seek(0)
+        content = self.err_log_file.read()
         offload_message = "[offloader]"
         self.assertIn(offload_message, content)
-        out_log_file.close()
-        err_log_file.close()
-        os.remove("./cache_out_log.txt")
-        os.remove("./cache_err_log.txt")
-
-        kill_process_tree(self.process.pid)
 
 
-class TestOffload1(CustomTestCase):
+class TestOffload2(CustomTestCase):
     """Testcase: Tests core functionality with --cpu-offload-gb configuration, inference requests successful.
     and the ingerence accuracy using the GSM8K dataset is no less than 0.86.
 
@@ -134,8 +144,9 @@ class TestOffload1(CustomTestCase):
     [Test Target] --offload-mode
     """
 
-    def test_max_loaded_loras_error(self):
-        other_args = [
+    @classmethod
+    def setUpClass(cls):
+        cls.other_args = [
             "--attention-backend",
             "ascend",
             "--disable-cuda-graph",
@@ -151,15 +162,25 @@ class TestOffload1(CustomTestCase):
             "sharded_gpu",
         ]
 
-        out_log_file = open("./cache_out_log.txt", "w+", encoding="utf-8")
-        err_log_file = open("./cache_err_log.txt", "w+", encoding="utf-8")
-        self.process = popen_launch_server(
+        cls.out_log_file = open("./cache_out_log.txt", "w+", encoding="utf-8")
+        cls.err_log_file = open("./cache_err_log.txt", "w+", encoding="utf-8")
+        cls.process = popen_launch_server(
             DEEPSEEK_V2_LITE_W8A8_WEIGHTS_PATH,
             DEFAULT_URL_FOR_TEST,
             timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
-            other_args=other_args,
-            return_stdout_stderr=(out_log_file, err_log_file),
+            other_args=cls.other_args,
+            return_stdout_stderr=(cls.out_log_file, cls.err_log_file),
         )
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.out_log_file.close()
+        cls.err_log_file.close()
+        os.remove("./cache_out_log.txt")
+        os.remove("./cache_err_log.txt")
+        kill_process_tree(cls.process.pid)
+
+    def test_max_loaded_loras_error(self):
         response = requests.post(
             f"{DEFAULT_URL_FOR_TEST}/generate",
             json={
@@ -172,16 +193,10 @@ class TestOffload1(CustomTestCase):
         )
         self.assertEqual(response.status_code, 200)
         self.assertIn("Paris", response.text)
-        err_log_file.seek(0)
-        content = err_log_file.read()
+        self.err_log_file.seek(0)
+        content = self.err_log_file.read()
         offload_message = "[offloader]"
         self.assertIn(offload_message, content)
-        out_log_file.close()
-        err_log_file.close()
-        os.remove("./cache_out_log.txt")
-        os.remove("./cache_err_log.txt")
-
-        kill_process_tree(self.process.pid)
 
 
 if __name__ == "__main__":
