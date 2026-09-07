86b17b86d11b96b56554f7e3f37ef394f5261fb7
YZY00Raiser <2452658175@qq.com>
2026-09-07 11:50:49 +0800

    update

--- test/registered/ascend/basic_function/1/831/test_npu_pp_single_node.py
+++ test/registered/ascend/basic_function/1/831/test_npu_pp_single_node.py
@@ -23,11 +23,13 @@ from sglang.test.test_utils import (
 )
 DEEPSEEK_CODER_V2_LITE_WEIGHTS_PATH="/home/weights/DeepSeek-Coder-V2-Lite-Instruct"
 LLAMA_3_1_8B_INSTRUCT_WEIGHTS_PATH="/home/weights/Llama-3.1-8B-Instruct"
+
+
 # Disable the fast input-logprobs path; use the reference log-softmax path.
 os.environ["SGLANG_ENABLE_FAST_INPUT_LOGPROBS"] = "false"
 register_npu_ci(est_time=10800, suite="full-16-npu-a3", nightly=True)
 
-'''
+
 class TestPPAccuracy(unittest.TestCase):
     """Test Case: Verify the accuracy of LLM models under TP+PP hybrid parallelism
 
@@ -210,7 +212,7 @@ class TestPPMixedChunk(CustomTestCase):
         self.assertGreater(metrics["score"], 0.74)
         time.sleep(4)
 
-'''
+
 class TestFixedBugs(unittest.TestCase):
     """Test Case: Verify normal inference under small batch size scenario with PP+chunked-prefill enabled
     [Test Category] Parameter
